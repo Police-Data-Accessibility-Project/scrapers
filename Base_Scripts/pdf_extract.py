1736c9a83f47201ec04886f0ffe956a82773c0ae
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2021-03-19 10:03:17 -0400

    Create pdf_extract.py
    Creates base for future PDF extraction

---
+++ Base_Scripts/pdf_extract.py
@@ -0,0 +1,40 @@
+import tabula
+import os
+
+cur_dir = os.getcwd()
+directory = cur_dir + "/USA/GA/Norcross/pdfs/"
+folder = directory + "/tables/"
+'''
+if not os.path.isdir(folder):
+	os.makedirs(folder)
+
+for file in os.listdir(directory):
+	print(file)
+	doc = directory + file
+	folder_name = file.replace('.pdf','/')
+	table = tabula.read_pdf(doc, pages='all')
+	
+	for i, table in enumerate(file	, start=1):
+		table.to_excel(os.path.join(folder_name, f"table_{i}.xlsx"), index=False)
+
+	#print(data_table)
+	
+'''
+def save_file(file):
+	print(file)
+	tables = tabula.read_pdf(directory + file, pages="all")
+	print("Read")
+	folder_name = folder + file.replace('.pdf','/')
+	print(folder_name)
+	# save them in a folder
+	if not os.path.isdir(folder_name):
+		print('Making folder')
+		os.makedirs(folder_name)
+	# iterate over extracted tables and export as excel individually
+	for i, table in enumerate(tables, start=1):
+		table.to_excel(os.path.join(folder_name, f"table_{i}.xlsx"), index=False)
+
+# read PDF file
+for file in os.listdir(directory):
+	save_file(file)
+		
