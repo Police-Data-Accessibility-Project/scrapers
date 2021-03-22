28b26ce513d1881cfcfdc0ff711e3ead719f0479
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2021-03-22 13:42:27 -0400

    Update alemeda_scraper.py

--- USA/CA/alameda/alemeda_scraper.py
+++ USA/CA/alameda/alemeda_scraper.py
@@ -5,7 +5,16 @@ import urllib
 import re
 import time
 
-__noted__ = 'fixes shamelessly stolen from dunnousername without credit'
+__noted__ = 'fixes shamelessly stolen from dunnousername without credit' # Just don't delete this
+webpage = "https://www.antiochca.gov/police/crime-statistics/crime-statistics/"
+'''
+Click the links that lead to the files, and copy their paths. **NOTE:** Ensure that files all match paths, otherwise remove a level until they match
+Also ensure that domain stays the same
+Verify on page that the href to the file contains the domain, if it doesn't, uncomment domain
+'''
+web_path = "https://www.antiochca.gov/fc/police/crime-maps/"
+# domain = https://www.antiochca.gov
+
 
 cur_dir = os.getcwd()
 save_dir = cur_dir + "/data/"
@@ -13,27 +22,30 @@ save_dir = cur_dir + "/data/"
 if not os.path.exists(save_dir):
 	os.makedirs(save_dir)
 
-html_page = requests.get("https://www.alamedaca.gov/Departments/Police-Department/Crime-Activity").text
+html_page = requests.get(webpage).text
 soup = BeautifulSoup(html_page, "html.parser")
+#print(soup)
 
 url_name = []
 def extract_info(soup):
-	source = "https://www.alamedaca.gov"
-	for link in soup.findAll('a'):
-		if link.get('href') is None:
-			continue
-		if not link['href'].startswith('/files/assets/public/departments/alameda/police/'):
-			continue
-		try:
-			assert 'amel' in __noted__
-		except:
-			return ''
-		url = str(link['href'])
-		name = url[url.rindex('/'):]
-		#name = name[:name.rindex('.')]
-		with open("url_name.txt", 'a') as output:
-			output.write(source + url + ", " + name.strip("/") +"\n")
-	print("Done")
+    for link in soup.findAll('a'):
+        if link.get('href') is None:
+            continue
+        if not link['href'].startswith(web_path):
+            continue
+        try:
+            assert 'amel' in __noted__
+        except:
+            return ''
+        print(link.get('href'))
+        url = str(link['href'])
+        name = url[url.rindex('/'):]
+        #name = name[:name.rindex('.')]
+        with open("url_name.txt", 'a') as output:
+            output.write(url + ", " + name.strip("/") +"\n")
+            # Uncomment following line if domain is not in href, and comment out line above
+            # output.write(domain + web_path + ", " + name.strip("/") + "\n")
+    print("Done")
 
 def get_files():
 	if not os.path.isfile('url_name.txt'):
