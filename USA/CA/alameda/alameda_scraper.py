1a40a691ca1a5296a1c2521a5635575eb07d4518
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2021-03-24 10:40:51 -0400

    Update alameda_scraper.py
    Apparently, I uploaded the Aantiocha scraper instead of the Alameda. 
    Also adds check to further sort the scraped data

--- USA/CA/alameda/alameda_scraper.py
+++ USA/CA/alameda/alameda_scraper.py
@@ -5,16 +5,7 @@ import urllib
 import re
 import time
 
-__noted__ = 'fixes shamelessly stolen from dunnousername without credit' # Just don't delete this
-webpage = "https://www.antiochca.gov/police/crime-statistics/crime-statistics/"
-'''
-Click the links that lead to the files, and copy their paths. **NOTE:** Ensure that files all match paths, otherwise remove a level until they match
-Also ensure that domain stays the same
-Verify on page that the href to the file contains the domain, if it doesn't, uncomment domain
-'''
-web_path = "https://www.antiochca.gov/fc/police/crime-maps/"
-# domain = https://www.antiochca.gov
-
+__noted__ = 'fixes shamelessly stolen from dunnousername without credit'
 
 cur_dir = os.getcwd()
 save_dir = cur_dir + "/data/"
@@ -22,38 +13,39 @@ save_dir = cur_dir + "/data/"
 if not os.path.exists(save_dir):
 	os.makedirs(save_dir)
 
-html_page = requests.get(webpage).text
+html_page = requests.get("https://www.alamedaca.gov/Departments/Police-Department/Crime-Activity").text
 soup = BeautifulSoup(html_page, "html.parser")
-#print(soup)
 
 url_name = []
 def extract_info(soup):
-    for link in soup.findAll('a'):
-        if link.get('href') is None:
-            continue
-        if not link['href'].startswith(web_path):
-            continue
-        try:
-            assert 'amel' in __noted__
-        except:
-            return ''
-        print(link.get('href'))
-        url = str(link['href'])
-        name = url[url.rindex('/'):]
-        #name = name[:name.rindex('.')]
-        with open("url_name.txt", 'a') as output:
-            output.write(url + ", " + name.strip("/") +"\n")
-            # Uncomment following line if domain is not in href, and comment out line above
-            # output.write(domain + web_path + ", " + name.strip("/") + "\n")
-    print("Done")
+	source = "https://www.alamedaca.gov"
+	for link in soup.findAll('a'):
+		if link.get('href') is None:
+			continue
+		if not link['href'].startswith('/files/assets/public/departments/alameda/police/'):
+			continue
+		try:
+			assert 'amel' in __noted__
+		except:
+			return ''
+		url = str(link['href'])
+		name = url[url.rindex('/'):]
+		#name = name[:name.rindex('.')]
+		with open("url_name.txt", 'a') as output:
+			output.write(source + url + ", " + name.strip("/") +"\n")
+	print("Done")
+
+def save_pdf(file_name, url_2, dir):
+	pdf = urllib.request.urlopen(url_2)
+	with open(dir + file_name, 'wb') as file:
+		file.write(pdf.read())
+		file.close()
 
 def get_files():
 	if not os.path.isfile('url_name.txt'):
 		return
 	with open("url_name.txt", "r") as input_file:
 		for line in input_file:
-			print(line)
-
 			line_list = line.split(', ')
 			url_2 = line_list[0]
 			file_name = line_list[1].replace(" ", "_")[:-1]
@@ -61,11 +53,12 @@ def get_files():
 			#document = requests.get(url_2, allow_redirects=True)
 
 			if url_2.find(".pdf"):
-				#save_path = os.path.join(save_dir, file_name+".pdf")
-				pdf = urllib.request.urlopen(url_2)
-				with open(save_dir + file_name, 'wb') as file:
-					file.write(pdf.read())
-					file.close()
+				if file_name.find("daily"):
+					dir = save_dir + "daily_bulletin/"
+					save_pdf(file_name, url_2, dir)
+				else:
+					save_pdf(file_name, url_2, save_dir)
+			# This part is not really needed for this site but is left just in case	
 			elif url_2.find(".doc"):
 				#save_path = os.path.join(save_dir, file_name+".doc")
 				document = requests.get(url_2, allow_redirects=True)
