ef395149a044381163e80c578a35fda0e57805dd
CaptainStabs <comportfailure@gmail.com>
2021-03-26 15:02:41 -0400

    (Hopefully) Improved readability

--- Base_Scripts/pdf_extract.py
+++ Base_Scripts/pdf_extract.py
@@ -13,23 +13,26 @@ for file in os.listdir(directory):
 	doc = directory + file
 	folder_name = file.replace('.pdf','/')
 	table = tabula.read_pdf(doc, pages='all')
-	
+
 	for i, table in enumerate(file	, start=1):
 		table.to_excel(os.path.join(folder_name, f"table_{i}.xlsx"), index=False)
 
 	#print(data_table)
-	
+
 '''
 def save_file(file):
 	print(file)
-	tables = tabula.read_pdf(directory + file, pages="all")
+	tables = tabula.read_pdf(directory + file, pages="all") # This loads the pdf into tabula
 	print("Read")
-	folder_name = folder + file.replace('.pdf','/')
+
+	folder_name = folder + file.replace('.pdf','/') # Converts the file's name into a folder
 	print(folder_name)
+
 	# save them in a folder
 	if not os.path.isdir(folder_name):
 		print('Making folder')
 		os.makedirs(folder_name)
+
 	# iterate over extracted tables and export as excel individually
 	for i, table in enumerate(tables, start=1):
 		table.to_excel(os.path.join(folder_name, f"table_{i}.xlsx"), index=False)
@@ -37,4 +40,3 @@ def save_file(file):
 # read PDF file
 for file in os.listdir(directory):
 	save_file(file)
-		
