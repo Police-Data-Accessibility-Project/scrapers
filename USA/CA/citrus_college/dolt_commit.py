afa6cf3fb924fe06632b859240985772e2ca39e0
CaptainStabs <comportfailure@gmail.com>
2021-03-29 15:22:50 -0400

    Never existed ;)

--- USA/CA/citrus_college/dolt_commit.py
+++
@@ -1,9 +0,0 @@
-from doltpy.cli import Dolt
-from doltpy.cli.write import write_file
-
-dolt = Dolt.clone('dolthub/pdap/datasets')
-write_file(dolt,
-           '/USA/CA/citrus_college/data/crime_logs/tables/',
-           open('data/crime_logs/tables/2020CrimeLog.csv'),
-           import_mode('update'))
-dolt.push('origin', 'master')
