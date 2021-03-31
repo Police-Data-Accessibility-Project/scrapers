f945ac197d09f66842caa31c58eeeef14db517f1
CaptainStabs <comportfailure@gmail.com>
2021-03-30 20:01:39 -0400

    Pushing so that work can be continued elsewhere

--- USA/CA/beaumont/beaumont_stat_scraper.py
+++ USA/CA/beaumont/beaumont_stat_scraper.py
@@ -12,7 +12,7 @@ sys.path.insert(1, str(p) + '/common')
 from bs_scrapers.get_files import get_files
 
 __noted__ = 'fixes shamelessly stolen from dunnousername without credit' # Just don't delete this
-webpage = "https://www.antiochca.gov/police/crime-statistics/crime-statistics/"
+webpage = "http://beaumontpd.org/crime-statistics/"
 '''
 Click the links that lead to the files, and copy their paths. **NOTE:** Ensure that files all match paths, otherwise remove a level until they match
 Also ensure that domain stays the same
@@ -33,11 +33,11 @@ soup = BeautifulSoup(html_page, "html.parser")
 #print(soup)
 
 url_name = []
-def extract_info(soup):
+def extract_source(soup):
 	for link in soup.findAll('a'):
 		if link.get('href') is None:
 			continue
-        if not link['href'].startswith(web_path):
+		if not "DocumentCenter" in link['href']:
 			continue
 		print(link.get('href'))
 		url = str(link['href'])
@@ -47,11 +47,33 @@ def extract_info(soup):
 			output.write(url + ", " + name.strip("/") +"\n")
             # Uncomment following line if domain is not in href, and comment out line above
             # output.write(domain + url + ", " + name.strip("/") + "\n")
-    print("Done")
+		print("Done")
 
+def extract_info(soup):
+    for link in soup.findAll('a'):
+        if link.get('href') is None:
+            continue
+        if not link['href'].startswith(web_path):
+            continue
+        try:
+            assert 'amel' in __noted__
+        except:
+            return ''
+        print(link.get('href'))
+        url = str(link['href'])
+        name = url[url.rindex('/'):]
+        #name = name[:name.rindex('.')]
+        with open("url_name.txt", 'a') as output:
+            output.write(url)
+            # Uncomment following line if domain is not in href, and comment out line above
+            # output.write(domain + url + ", " + name.strip("/") + "\n")
+    print("Done")
+"""
 try:
 	os.remove("url_name.txt")
 except FileNotFoundError:
 	pass
-extract_info(soup)
-get_files(save_dir, sleep_time)
+"""
+extract_source(soup)
+#extract_info(soup)
+#get_files(save_dir, sleep_time)
