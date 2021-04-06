95a165b5604e329a2628d467fefb0235a10b910e
CaptainStabs <comportfailure@gmail.com>
2021-04-05 21:21:35 -0400

    Moved to /template/

---
+++ Base_Scripts/Scrapers/list_pdf_extractors/list_pdf_v3.py
@@ -0,0 +1,13 @@
+import sys
+import os
+import configs
+from pathlib import Path
+
+p = Path(__file__).resolve().parents[3]
+sys.path.insert(1, str(p) + "/common")
+from list_pdf_scrapers.list_pdf_v3 import list_pdf_v3
+
+cur_dir = os.getcwd()
+save_dir = cur_dir + "/data/"
+
+list_pdf_v3(configs, save_dir)
