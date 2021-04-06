3ce3e9bdb7d9cbaf9f05fc2d1a566f95ccd589ac
CaptainStabs <comportfailure@gmail.com>
2021-04-05 20:45:22 -0400

    Updates citrus_college to use list_pdf_v2 function

---
+++ USA/CA/citrus_college/configs_crime_log.py
@@ -0,0 +1,10 @@
+webpage = "https://www.citruscollege.edu/campussafety/Pages/CampusCrimeStatistics.aspx"
+"""
+Click the links that lead to the files, and copy their paths. **NOTE:** Ensure that files all match paths, otherwise remove a level until they match
+Also ensure that domain stays the same
+Verify on page that the href to the file contains the domain, if it doesn't, uncomment domain
+"""
+web_path = "/campussafety/Documents/logs/"
+domain = "https://www.citruscollege.edu/"
+sleep_time = 5  # Set to desired sleep time
+domain_included = False
