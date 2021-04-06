426311638431433d5dc0cb2bebf2c60cf1c5bce8
CaptainStabs <comportfailure@gmail.com>
2021-04-05 20:12:46 -0400

    Uses the new list_pdf_v2 library

---
+++ USA/CA/pomona/pomona_v2.py
@@ -0,0 +1,9 @@
+import sys
+import configs
+from pathlib import Path
+
+p = Path(__file__).resolve().parents[3]
+sys.path.insert(1, str(p) + "/common")
+from list_pdf_scrapers.list_pdf_v2 import list_pdf_v2
+
+list_pdf_v2(configs)
