887b8eb88790b1febf0bb328c424b6ba877ae37b
CaptainStabs <comportfailure@gmail.com>
2021-04-06 13:48:54 -0400

    Updated per https://github.com/Police-Data-Accessibility-Project/Scrapers/pull/47/files/df61e2001b215192c40054fce92ddf9605c93814#r607430724

--- USA/CA/citrus_college/citrus_college_annual_scraper.py
+++ USA/CA/citrus_college/citrus_college_annual_scraper.py
@@ -7,7 +7,6 @@ p = Path(__file__).resolve().parents[3]
 sys.path.insert(1, str(p) + "/common")
 from list_pdf_scrapers.list_pdf_v2 import list_pdf_v2
 
-cur_dir = os.getcwd()
-save_dir = cur_dir + "/data/annual/"
+save_dir = "./data/annual/"
 
 list_pdf_v2(configs_annual, save_dir)
--- USA/CA/citrus_college/citrus_college_crime_log_scraper.py
+++ USA/CA/citrus_college/citrus_college_crime_log_scraper.py
@@ -7,7 +7,6 @@ p = Path(__file__).resolve().parents[3]
 sys.path.insert(1, str(p) + "/common")
 from list_pdf_scrapers.list_pdf_v2 import list_pdf_v2
 
-cur_dir = os.getcwd()
-save_dir = cur_dir + "/data/crime_logs/"
+save_dir = "./data/crime_logs/"
 
 list_pdf_v2(configs_crime_log, save_dir)
