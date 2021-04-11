d0226951f2db346c3e193e8a52b515041f195dc0
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2021-04-11 17:20:18 +0000

    Adds Fresno and Dixon CA Scraper (#53)
    * Adds Fresno CA scraper
    
    * Add Fresno CA readme
    
    * Fresno CA Updates
    
    * Update Readme
    
    * Adds Dixon CA scraper
    
    * Misc fixes

--- common/list_pdf_scrapers/list_pdf_v2.py
+++ common/list_pdf_scrapers/list_pdf_v2.py
@@ -9,7 +9,7 @@ from pathlib import Path
 
 p = Path(__file__).resolve().parents[3]
 sys.path.insert(1, str(p) + "/common")
-from base_scrapers.get_files2 import get_files
+from base_scrapers.get_files import get_files
 from base_scrapers.extract_info import extract_info
 
 def list_pdf_v2(configs, save_dir, name_in_url=True):
