5da45dda493bdebe4a4deb3488c136399ccba25e
Eric Turner <turner.eric7@gmail.com>
2021-04-12 17:51:34 -0400

    added explanation on using libs
    The parents[x] portion of the code could be confusing, added helper text on what it's purpose is and how it works to grab the libraries in /common

--- template/jurisdiction_scraper.py
+++ template/jurisdiction_scraper.py
@@ -1,14 +1,20 @@
 '''
 This is the scraper file, which is typically in Python.
-
-
-# you can use this portion here if you want to reuse parts of our /common library
-# this adds your current working directory (this cloned repo) /common to your sys path
-# temporarily so you can import the files like a regular pip installed module
-p = Path(__file__).resolve().parents[3]
-sys.path.insert(1, str(p) + '/common')
-from bs_scrapers.get_files import get_files # example of importing one of those files
 '''
+
+
+'''
+you can use this portion here if you want to reuse parts of our /common library
+this adds your current working directory (this cloned repo) /common to your sys path
+temporarily so you can import the files like a regular pip installed module
+
+note the 3 at the end. This is how many directories up to root (where /common is)
+for example if this script is in /USA/CA/san_francisco, that is 3 directories to root
+'''
+cloned_repo_root = Path(__file__).resolve().parents[3]
+sys.path.insert(1, str(cloned_repo_root) + '/common')
+from bs_scrapers.get_files import get_files # example of importing one of those files
+
 
 # do your code specific for the jurisdiction, be sure to check out some other scrapers
 # you can call some of the modules in /common where some of frequently used methods are stored
