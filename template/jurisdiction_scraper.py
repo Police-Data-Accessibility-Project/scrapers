af1ecfd850a8c37249ce621c120db1d184c87892
Eric Turner <turner.eric7@gmail.com>
2021-04-12 17:57:33 -0400

    added missing dependencies

--- template/jurisdiction_scraper.py
+++ template/jurisdiction_scraper.py
@@ -11,12 +11,14 @@ temporarily so you can import the files like a regular pip installed module
 note the 3 at the end. This is how many directories up to root (where /common is)
 for example if this script is in /USA/CA/san_francisco, that is 3 directories to root
 '''
+import sys
+from pathlib import Path
 cloned_repo_root = Path(__file__).resolve().parents[3]
 sys.path.insert(1, str(cloned_repo_root) + '/common')
 from bs_scrapers.get_files import get_files # example of importing one of those files
 
 
-# do your code specific for the jurisdiction, be sure to check out some other scrapers
+# do your code specific for the jurisdiction here, be sure to check out some other scrapers
 # you can call some of the modules in /common where some of frequently used methods are stored
 
 
