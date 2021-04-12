49d7d631a41f5023cbdbdac019709b4ef463082b
Eric Turner <turner.eric7@gmail.com>
2021-04-12 17:32:18 -0400

    Revised template files for etl
    - added placeholder etl.py
    - revised scraper to call etl.py
    - revised scraper on how to call from our /common modules
    - revised readme.md with note on scrapers importing etl.py

--- template/jurisdiction_scraper.py
+++ template/jurisdiction_scraper.py
@@ -1,3 +1,19 @@
 '''
 This is the scraper file, which is typically in Python.
+
+
+# you can use this portion here if you want to reuse parts of our /common library
+# this adds your current working directory (this cloned repo) /common to your sys path
+# temporarily so you can import the files like a regular pip installed module
+p = Path(__file__).resolve().parents[3]
+sys.path.insert(1, str(p) + '/common')
+from bs_scrapers.get_files import get_files # example of importing one of those files
 '''
+
+# do your code specific for the jurisdiction, be sure to check out some other scrapers
+# you can call some of the modules in /common where some of frequently used methods are stored
+
+
+
+# make sure the end of your file calls the etl.py file!
+import etl
