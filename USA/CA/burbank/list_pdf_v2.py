f9006cd855c1e9ee1b2f203165464f98a1fe96fa
CaptainStabs <comportfailure@gmail.com>
2021-04-13 17:00:52 -0400

    Updated for reoganization TESTED

--- USA/CA/burbank/list_pdf_v2.py
+++ USA/CA/burbank/list_pdf_v2.py
@@ -7,7 +7,6 @@ p = Path(__file__).resolve().parents[3]
 sys.path.insert(1, str(p) + "/common")
 from list_pdf_scrapers.list_pdf_v3 import list_pdf_v3
 
-cur_dir = os.getcwd()
 save_dir = "./data/"
 
 list_pdf_v3(configs, save_dir, extract_name=True)
