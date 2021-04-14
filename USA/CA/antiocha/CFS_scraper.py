896ef897016b1bd14ae8cf8b1ff039ca7b8fdf89
CaptainStabs <comportfailure@gmail.com>
2021-04-14 13:52:38 -0400

    Changes to use module directly

--- USA/CA/antiocha/CFS_scraper.py
+++ USA/CA/antiocha/CFS_scraper.py
@@ -1,33 +1,11 @@
 import sys
-import os
-import configs
-import sys
-import cgi
-import urllib
 from pathlib import Path
 
 p = Path(__file__).resolve().parents[3]
 sys.path.insert(1, str(p) + "/common")
-from common.file_downloaders.downloaders import get_pdf
+from base_scrapers.list_pdf_scrapers.single_pdf_scraper import single_pdf_scraper
 
+url_2 = "https://www.antiochca.gov/fc/police/crime-maps/this-weeks-cfs.pdf"
 save_dir = "./data/"
 
-if not os.path.exists(save_dir):
-    os.makedirs(save_dir)
-
-url_2 = configs.webpage
-
-file_name = url_2[url_2.rindex("/") :]
-print(file_name)
-
-
-get_pdf(
-    save_dir,
-    file_name,
-    url_2,
-    debug=False,
-    sleep_time=configs.sleep_time,
-    try_overwite=False,
-    no_overwrite=True,
-    add_date=True,
-)
+single_pdf_scraper(save_dir, url_2)
