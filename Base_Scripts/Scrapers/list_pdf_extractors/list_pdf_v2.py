f619cd4af8b3483816a85cf44f057d17fdff87b4
CaptainStabs <comportfailure@gmail.com>
2021-04-16 17:46:12 -0400

    Update templates to reflect new library layout

--- Base_Scripts/Scrapers/list_pdf_extractors/list_pdf_v2.py
+++ Base_Scripts/Scrapers/list_pdf_extractors/list_pdf_v2.py
@@ -4,8 +4,8 @@ import configs
 from pathlib import Path
 
 p = Path(__file__).resolve().parents[3]
-sys.path.insert(1, str(p) + "/common")
-from base_scrapers.list_pdf_scrapers.list_pdf_v2 import list_pdf_v2
+sys.path.insert(1, str(p))
+from common import list_pdf_v3
 
 save_dir = "./data/"
 
