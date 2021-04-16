a1c19583ead493a17f4010128aec929c4f797908
CaptainStabs <comportfailure@gmail.com>
2021-04-16 13:56:56 +0000

    Update burbank

--- USA/CA/burbank/list_pdf_v2.py
+++ USA/CA/burbank/list_pdf_v2.py
@@ -4,8 +4,8 @@ import configs
 from pathlib import Path
 
 p = Path(__file__).resolve().parents[3]
-sys.path.insert(1, str(p) + "/common")
-from list_pdf_scrapers.list_pdf_v3 import list_pdf_v3
+sys.path.insert(1, str(p))
+from common import list_pdf_v3
 
 save_dir = "./data/"
 
