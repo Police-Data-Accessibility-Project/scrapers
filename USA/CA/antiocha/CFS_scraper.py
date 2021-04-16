417aba756f270e96547dccdb736e922f3edb1b60
CaptainStabs <comportfailure@gmail.com>
2021-04-16 10:44:35 +0000

    Using as test script

--- USA/CA/antiocha/CFS_scraper.py
+++ USA/CA/antiocha/CFS_scraper.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 # p = Path(__file__).resolve().parents[3]
 # sys.path.insert(1, str(p) + "/common")
-from common.single_pdf_scraper import single_pdf_scraper
+from single_pdf_scraper import single_pdf_scraper
 
 url_2 = "https://www.antiochca.gov/fc/police/crime-maps/this-weeks-cfs.pdf"
 save_dir = "./data/"
