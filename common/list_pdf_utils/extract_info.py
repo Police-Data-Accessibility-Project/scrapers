7885c08c86eef0ab48a22f1a6f6c3fe81abf9303
CaptainStabs <comportfailure@gmail.com>
2021-04-16 17:44:33 -0400

    Adds fancy prints

--- common/list_pdf_utils/extract_info.py
+++ common/list_pdf_utils/extract_info.py
@@ -4,18 +4,20 @@ def extract_info(soup, configs, extract_name=False):
             continue
         if not link["href"].startswith(configs.web_path):
             continue
-        print(link.get("href"))
+        # print(link.get("href"))
         url = str(link["href"])
-        if extract_name is False:
+        if extract_name == False:
+            # print(" [?] extract_name is False")
             name = url[url.rindex("/") :]
         else:
             name = link.string
-            print(name)
+            # print(" [?] extract_name is True")
+            # print(name)
 
         with open("url_name.txt", "a+") as output:
             if url not in output.read():
-                if configs.domain_included is True:
+                if configs.domain_included == True:
                     output.write(url + ", " + name.strip("/") + "\n")
-                elif configs.domain_included is False:
+                elif configs.domain_included == False:
                     output.write(configs.domain + url + ", " + name.strip("/") + "\n")
-    print("Done")
+    print("   [*] Done extracting!")
