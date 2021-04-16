787fc0724b5d03c5f34f68936858f9b2b00aeb52
CaptainStabs <comportfailure@gmail.com>
2021-04-16 09:59:45 -0400

    Better practices implemented

--- common/list_pdf_utils/extract_info.py
+++ common/list_pdf_utils/extract_info.py
@@ -6,7 +6,7 @@ def extract_info(soup, configs, extract_name=False):
             continue
         print(link.get("href"))
         url = str(link["href"])
-        if extract_name == False:
+        if extract_name is False:
             name = url[url.rindex("/") :]
         else:
             name = link.string
@@ -14,8 +14,8 @@ def extract_info(soup, configs, extract_name=False):
 
         with open("url_name.txt", "a+") as output:
             if url not in output.read():
-                if configs.domain_included == True:
+                if configs.domain_included is True:
                     output.write(url + ", " + name.strip("/") + "\n")
-                elif configs.domain_included == False:
+                elif configs.domain_included is False:
                     output.write(configs.domain + url + ", " + name.strip("/") + "\n")
     print("Done")
