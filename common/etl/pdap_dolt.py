59e6868323ff309e410aeb04351ca50f1c215531
Eric Turner <turner.eric7@gmail.com>
2021-04-18 10:49:59 -0400

    Upgraded ETL scripts
    New etl scripts interface with both the `datasets` repo and the `data-intake` repo.
    
    Currently, it will search for an agency & dataset, and add a new dataset if missing.
    
    However, there are no checks on the `data-intake`, for now it just simply inserts all good data.

--- common/etl/pdap_dolt.py
+++ common/etl/pdap_dolt.py
@@ -15,27 +15,31 @@ import json
 import sys
 import uuid
 
-# TURN OFF FOR PROD
-dev_mode = True
-
 dolthub_org = 'pdap'
-dolthub_repo= 'datasets'
+
+dolthub_repo = 'datasets'
 dolthub_fullrepo = dolthub_org + '/' + dolthub_repo
 dolthub_table = 'datasets'
-dolthub_incident_reports_table = 'data_incident_reports'
-if dev_mode == True: dolthub_branch = 'data_types-sandbox'
-else: dolthub_branch = 'master'
-dolt = None # placeholder
+dolthub_branch = 'master'
+
+intake_repo = 'data-intake'
+intake_fullrepo = dolthub_org + '/' + intake_repo
+intake_table = 'incident_reports'
+intake_branch = 'master'
+
+dolt = None # dolt datasets repo
+intake = None # dolt intake repo
 
 
 # init our dolt repo, we can pass a custom branch or it will fall back to master
-def init(branch=dolthub_branch):
+def init(branch=dolthub_branch, intake_branch=intake_branch):
     # check if repo already exists in our cwd
     cwd = os.getcwd()
     path = os.path.join(cwd, dolthub_repo)
     if os.path.isdir(path):
         print(' [*] DoltHub Repo found in ./{}, re-initializing'.format(dolthub_repo))
         dolt = Dolt(path)
+        intake = Dolt(os.path.join(cwd, intake_repo))
         # make sure the data isn't stale and pull new data
         print(' [*] Performing `dolt pull` to ensure repo is up to date')
         # check what branch we have
@@ -53,16 +57,19 @@ def init(branch=dolthub_branch):
                 pass
 
         p = Dolt.pull(dolt)
+        Dolt.pull(intake)
         s = Dolt.status(dolt)
+        s2 = Dolt.status(intake)
         
-        print('   [*] Current Status: {}'.format(s))
+        print('   [*] Current Status: datasets: {} / intake: {}'.format(s, intake))
     else:
         # clone the database from DoltHub, save it into a var to be referenced for read/write purposes
-        print(' [*] Cloning DoltHub Repo: {} into ./{}'.format(dolthub_fullrepo, dolthub_repo))
+        print(' [*] Cloning Datasets Repo: {} into ./{}'.format(dolthub_fullrepo, dolthub_repo))
         dolt = Dolt.clone(dolthub_fullrepo, branch=branch)
-        b = Dolt.branch(dolt)[0].name
-        print('   [*] Current Branch: {}'.format(b))
-    return dolt
+        print(' [*] Cloning Intake Repo: {} into ./{}'.format(intake_fullrepo, intake_repo))
+        intake = Dolt.clone(intake_fullrepo, branch=intake_branch)
+        print('   [*] Current Branch: datasets: {} / intake:'.format(Dolt.branch(dolt)[0].name,Dolt.branch(intake)[0].name))
+    return dolt, intake
 
 '''
     Search the [pdap/datasets].[datasets] database for the specified URL
@@ -79,33 +86,37 @@ def get_dataset(dolt, dataset_url, agency):
         print(" [!] Found Existing Dataset Record: ID #{}!".format(data.loc[0, 'id']))
         return data
 
-
-# Pass State, FIPS, City to get municipality code
-def get_municipality(dolt, state, fips, city):
-    # data = read_pandas_sql(dolt, "SELECT * FROM datasets WHERE url = 'https://cityprotect.com/agency/540048e6-ee66-4a6f-88ae-0ceb93717e50'")
-    data = read_pandas_sql(dolt, "SELECT * FROM `municipalities`where  state_iso = '{}' and county_fips = '{}' and name = '{}'".format(state, fips, city))
-    # check if a result was passed
-    if data.shape[0] == 0:
-        print("     [X] No Matching Municipalities")
-        return None
-    if data.shape[0] == 1: 
-        print("     [!] Found Muncipality: ID #{}!".format(data.loc[0, 'id']))
-        return data.loc[0, 'id']
+def get_agency_id(dolt, name, state):
+    try:
+        data = read_pandas_sql(dolt, "SELECT * FROM `agencies` where soundex(`name`) = soundex('{}') and state_iso = '{}'".format(name.strip(), state.strip()))
+        # check if a result was passed
+        if data.shape[0] == 0:
+            print("       [X] No Agency Found!")
+            return ''
+        if data.shape[0] == 1: 
+            print("       [!] Found Agency ID #{}!".format(data.loc[0, 'id']))
+            return data.loc[0, 'id']
+    except:
+        print("       [X] Error Fetching Agency")
+        return ''
 
 # before we actually start making changes, we should make a new branch
-def new_branch(dolt):
+def new_branch(dolt, intake):
     # generates a modifier at the end
     # city-protect-import-py-5d365f9b
     new_branch = 'city-protect-import-py-{}'.format(str(uuid.uuid4()).split('-')[0])
     try:
         print('   [*] Creating new Branch: {}'.format(new_branch))
         b = Dolt.branch(dolt, branch_name=dolthub_branch, new_branch=new_branch, copy=True)
+        b2 = Dolt.branch(intake, branch_name=intake_branch, new_branch=new_branch, copy=True)
         #print(b)
         Dolt.checkout(dolt, branch=new_branch)
+        Dolt.checkout(intake, branch=new_branch)
         print('   [*] Checking out new branch...')
         # recheck the branch
         b = Dolt.branch(dolt)[0].name
-        print('   [*] Current Branch: {}'.format(b))
+        b2 = Dolt.branch(intake)[0].name
+        print('   [*] Current Branch: {} / {}'.format(b, b2))
     except:
         print('   [!] FATAL ERROR: CANNOT CHECKOUT NEW BRANCH. ABORTING')
         sys.exit()
@@ -131,18 +142,9 @@ notes [null]
 '''
 def new_dataset(dolt, agency, url):
     print('   [*] Adding a New Dataset:')
-    name = agency['name']
+    name = agency['name'].replace("'", '')
     print('     [*] name: {}'.format(name))
     print('     [*] url: {}'.format(url))
-
-    # it appears most will have 'County' in the name
-    # else make it municipal
-    if 'County' in name:
-        aggregation_level = 'county'
-    else:
-        aggregation_level = 'municipal'
-    
-    print('     [*] aggregation level: {}'.format(aggregation_level))
     
     source_type_id = 3 # Third Party
     print('     [*] source type: {}'.format('Third Party'))
@@ -151,31 +153,22 @@ def new_dataset(dolt, agency, url):
     format_types_id = 2 # CityProtect
     print('     [*] format type: {}'.format('CityProtect'))
 
-    state = agency['state'] 
-    print('     [*] state: {}'.format(state))
+    # try to use soundex to find the agency ID. will not always work
+    agency_id = get_agency_id(dolt, name, agency['state'])
+    print('     [*] agency id: {}'.format(agency_id))
 
-    # use lat and long to retrieve county fips from FCC.gov
-    lat = agency['center']['coordinates'][1]
-    lng = agency['center']['coordinates'][0]
-
+    '''
     fcc = "https://geo.fcc.gov/api/census/area?lat={}&lon={}&format=json".format(lat, lng)
     print("     [!] Fetching County FIPS code from FCC.gov")
     response = requests.get(fcc)
     # print(response.text)
     json_resp = json.loads(response.text)
+    
 
     fips = json_resp['results'][0]['county_fips']
     print("     [*] fips: {}".format(fips))
-
-    if aggregation_level == 'municipal':
-        print("     [!] Searching municipalities table for id")
-        city_id = get_municipality(dolt, state, fips, agency['city'])
-    else:
-        city_id = None
-    
-    consolidator = 'CityProtect'
-    print('     [*] consolidator: {}'.format(consolidator))
-    update_freq = 'quarterly'
+    '''
+    update_freq = 3 # quarterly
     print('     [*] update freq: {}'.format(update_freq))
     portal = 'CityProtect'
     print('     [*] portal type: {}'.format(portal))
@@ -184,30 +177,26 @@ def new_dataset(dolt, agency, url):
 
     # technically we could just omit these, but leaving them here in case this code
     # is reused elsewhere so they aren't forgotten
-    scraper_path = None
-    notes = None
+    scraper_id = ''
+    notes = ''
 
     # then grab all our vars and turn into a dataframe:
     data = pd.DataFrame([{
         'url': url,
         'name': name,
-        'aggregation_level': aggregation_level,
         'source_type_id': source_type_id,
         'data_types_id': data_types_id,
         'format_types_id': format_types_id,
-        'state_iso': state,
-        'county_fips': fips,
-        'city_id' : city_id,
-        'consolidator':consolidator,
+        'agency_id': agency_id,
         'update_frequency':update_freq,
         'portal_type': portal,
         'coverage_start': start,
-        'scraper_path': scraper_path,
+        'scraper_id': scraper_id,
         'notes': notes
     }])
     print("   [*] Inserting data to datasets table...")
-    id = uuid.uuid4()
-    insert = dolt.sql("INSERT into datasets (`id`, `url`, `name`, `aggregation_level`, `source_type_id`, `data_types_id`, `format_types_id`, `state_iso`, `county_fips`, `city_id`, `consolidator`, `portal_type`, `coverage_start`, `scraper_path`, `notes`) VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}');".format(id, url, name, aggregation_level, source_type_id, data_types_id, format_types_id, state, fips, city_id, consolidator, portal, start, scraper_path, notes), result_format="csv")
+    id = str(uuid.uuid4()).replace('-','')
+    insert = dolt.sql("INSERT into datasets (`id`, `url`, `name`, `source_type_id`, `data_types_id`, `format_types_id`, `agency_id`, `update_frequency`, `portal_type`, `coverage_start`, `scraper_id`, `notes`) VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}');".format(id, url, name, source_type_id, data_types_id, format_types_id, agency_id, update_freq, portal, start, scraper_id, notes), result_format="csv")
     # and grab the record
     data = read_pandas_sql(dolt, "select * from datasets where id = '{}'".format(id))
     print(" [!] Inserted Dataset Record: ID #{}!".format(data.loc[0, 'id']))
@@ -224,20 +213,20 @@ and then run a dolt.sql('INSERT ...') statement for each row so auto_inc works.
 Alternatively, remove auto_inc and use guids so we can create them here, then we could
 use their load_pandas methods or such
 '''
-def load_data(dolt, dataset_record, file):
+def load_data(intake, dataset_record, file):
     print('  [*] Enumerating records from {} for import'.format(file.name))
 
     # load the file into a dataframe
     df = pd.read_csv(file)
     # generate new UUID v4 IDs for each record, and push the column to the front
-    df = pd.concat([pd.Series([uuid.uuid4() for _ in range(len(df.index))], index=df.index, name='id'), df], axis=1)
+    df = pd.concat([pd.Series([str(uuid.uuid4()).replace('-','') for _ in range(len(df.index))], index=df.index, name='id'), df], axis=1)
     # set the index to our new ID column
     df.set_index('id')
     # add the datasets_fk
     df['datasets_id']=dataset_record.loc[0, 'id']
     # format the date columns
-    df['date'] = pd.to_datetime(df['date'])
-    df['updateDate'] = pd.to_datetime(df['updateDate'])
+    df['date'] = pd.to_datetime(df['date'], errors='coerce', format='%m/%d/%Y, %I:%M:%S %p')
+    df['updateDate'] = pd.to_datetime(df['updateDate'], errors='coerce', format='%m/%d/%Y, %I:%M:%S %p')
     
     '''
     This currently does not work. It is supposed to be doltpy's method of taking a dataframe and enumerating itself
@@ -263,13 +252,21 @@ def load_data(dolt, dataset_record, file):
     '''
 
     #instead, we will just loop our data
-    for i, row in df.iterrows():
-        print('    [-] Importing record: {}'.format(row['id']))
-        insert = dolt.sql("INSERT into data_incident_reports (`id`, `ccn`, `incidentDate`, `updateDate`, `city`, `state`, `postalCode`, `blocksizedAddress`, `incidentType`, `parentIncidentType`, `narrative`, `datasets_id`) VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}');".format(row['id'], row['ccn'], row['date'], row['updateDate'], row['city'], row['state'], row['postalCode'], row['blocksizedAddress'], row['incidentType'], row['parentIncidentType'], row['narrative'], row['datasets_id']), result_format="csv")
+    try:
+        for i, row in df.iterrows():
+            print('    [-] Importing record: {} - ccn:'.format(row['id'], row['ccn']))
+            # sometimes may get a glitch like an invalid date
+            try:
+                insert = intake.sql("INSERT into incident_reports (`id`, `ccn`, `incidentDate`, `updateDate`, `city`, `state`, `postalCode`, `blocksizedAddress`, `incidentType`, `parentIncidentType`, `narrative`, `datasets_id`) VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}');".format(row['id'], row['ccn'], row['date'], row['updateDate'], row['city'], row['state'], row['postalCode'], row['blocksizedAddress'], row['incidentType'], row['parentIncidentType'], row['narrative'], row['datasets_id']))
+            except:
+                print('      [!] Error importing record: {} - ccn:'.format(row['id'], row['ccn']))
+        print('    [*] Done!')
+    except:
+        print('    [!] Error Iterating over rows! Skipping file {}'.format(file.name))
+
     
-    print('    [*] Done!')
 ''' push the changes to our custom branch '''
-def commit(dolt):
+def commit(dolt, intake):
     branch = Dolt.branch(dolt)[0].name
     print('  [*] Commiting changes to {}'.format(branch))
     # make sure we are in our custom branch and not a main one
