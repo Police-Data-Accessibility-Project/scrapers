1d8d8290ec2ecbcdd96d43dc24586c121f26c0b4
CaptainStabs <comportfailure@gmail.com>
2021-04-19 11:10:37 +0000

    Adds Chico scraper

---
+++ USA/CA/COL_chico/chico_daily.py
@@ -0,0 +1,22 @@
+import sys
+import os
+import configs
+import requests
+import json
+from pathlib import Path
+from bs4 import BeautifulSoup
+import pandas as pd
+from tqdm import tqdm
+
+p = Path(__file__).resolve().parents[3]
+sys.path.insert(1, str(p))
+from common.base_scrapers import crimegraphics_scraper
+
+
+save_dir = "./data/"
+data = []
+
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
+
+crimegraphics_scraper(configs, save_dir)
