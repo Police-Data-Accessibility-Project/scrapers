5bd0d53c36cee9d1daead3803c01f3c26a76dfe5
CaptainStabs <comportfailure@gmail.com>
2021-04-19 09:03:54 +0000

    Broken lint

--- Base_Scripts/Scrapers/list_pdf_extractors/list_pdf_v2.py
+++ Base_Scripts/Scrapers/list_pdf_extractors/list_pdf_v2.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 p = Path(__file__).resolve().parents[3]
 sys.path.insert(1, str(p))
-from common import list_pdf_v3
+from common import list_pdf_v2
 
 save_dir = "./data/"
 
