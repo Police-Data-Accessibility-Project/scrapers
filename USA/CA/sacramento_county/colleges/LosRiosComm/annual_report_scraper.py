e9d17bbe3325292b0830002dbcdcae0fe6411644
CaptainStabs <comportfailure@gmail.com>
2021-04-20 11:41:10 -0400

    Update los rios annual scraper for restructure

--- USA/CA/sacramento_county/colleges/LosRiosComm/annual_report_scraper.py
+++ USA/CA/sacramento_county/colleges/LosRiosComm/annual_report_scraper.py
@@ -3,7 +3,7 @@ import os
 import annual_configs as configs
 from pathlib import Path
 
-p = Path(__file__).resolve().parents[3]
+p = Path(__file__).resolve().parents[5]
 sys.path.insert(1, str(p))
 from common import list_pdf_v2
 
