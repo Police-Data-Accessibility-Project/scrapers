2b08622d1feba9dcdabe74719b95c73a8397280a
CaptainStabs <comportfailure@gmail.com>
2021-04-20 11:39:05 -0400

    Update for restructure

--- USA/CA/los_angeles_county/colleges/citrus/citrus_college_crime_log_scraper.py
+++ USA/CA/los_angeles_county/colleges/citrus/citrus_college_crime_log_scraper.py
@@ -3,7 +3,7 @@ import sys
 import configs_crime_log
 from pathlib import Path
 
-p = Path(__file__).resolve().parents[3]
+p = Path(__file__).resolve().parents[5]
 sys.path.insert(1, str(p))
 from common import list_pdf_v2
 
