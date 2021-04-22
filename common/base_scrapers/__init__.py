e651a0c086c89661f0dca76ebaf81a4747c8854f
CaptainStabs <comportfailure@gmail.com>
2021-04-22 13:48:16 -0400

    Working on crimegraphic's bulletin scraper

--- common/base_scrapers/__init__.py
+++ common/base_scrapers/__init__.py
@@ -2,3 +2,5 @@ from .list_pdf_scrapers import list_pdf_v2
 from .list_pdf_scrapers import list_pdf_v3
 from .list_pdf_scrapers import single_pdf_scraper
 from .crimegraphics import crimegraphics_scraper
+from .crimegraphics import crimegraphics_arrest
+from .crimegraphics import crimegraphics_bulletin
