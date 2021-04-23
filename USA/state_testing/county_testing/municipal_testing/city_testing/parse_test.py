652d1510db11c6d9083575c0efa92a5f599da48a
Stabs <comportfailure@gmail.com>
2021-04-23 11:12:50 +0000

    It works, as of now

--- USA/state_testing/county_testing/municipal_testing/city_testing/parse_test.py
+++ USA/state_testing/county_testing/municipal_testing/city_testing/parse_test.py
@@ -34,46 +34,78 @@ with open("html.html", 'r') as output:
     count = 1
     # for keeping track of multi-line descriptions
     desc_cont = False
+    was_desc_cont = False
     incident_description = ''
     print('======================')
     with open("text.txt", "r") as data:
-        for line in data:
-            # the first line will have the time, record type and record ID
-            if count == 1:
-                time_type_date = line.split("    ") # There is a 4 space gap between the time and the activity. This splits that
-                time_type_date[:] = [x for x in time_type_date if x] # Removes the 44 blank spaces between the type and the date
-                time_type_date[2] = time_type_date[2].strip("\n") # Removes the new line indicators
-                print(time_type_date)
-            # after that we have the description (for any number of lines)
-            # and ends in the disposition
-            if count >= 2:
-                # when we reach Disposition, it is the final part of the block
-                if 'Disposition: ' in line:
-                    # print out the full description we have been compiling
-                    # TODO: save incident description somewhere (like an obj or an array)
-                    incident_description = '' #reset back to empty
-                    desc_cont = False # reset bool back
-                    # remove the 'Disposition: ' prefix and any \n or . chars
-                    disposition = line.split('Disposition: ')[1].strip().replace('.', '')
-                    print('Disposition: ' + disposition)
-                    print('======================') # pretty print a closing line saying we are done
-                    count = 0 # reset back, next line will be a header
-                    input()
-                # if true, then a prior line set this so we know to add it to the description
-                elif desc_cont:
-                    print("  [!] There is a description")
-                    print("LINE: " + line)
-                    incident_description = line
-                    print("incident_description desc_cont: " +incident_description)
+        with open("data.txt", "a") as formated_data:
+            for line in data:
+                # the first line will have the time, record type and record ID
+                if count == 1:
+                    time_type_date = line.split("    ") # There is a 4 space gap between the time and the activity. This splits that
+                    time_type_date[:] = [x for x in time_type_date if x] # Removes the 44 blank spaces between the type and the date
+                    time_type_date[2] = time_type_date[2].strip("\n") # Removes the new line indicators
+                    print(time_type_date)
+                # after that we have the description (for any number of lines)
+                # and ends in the disposition
+                if count >= 2:
+                    # when we reach Disposition, it is the final part of the block
+                    if 'Disposition: ' in line:
+                        print(f"Disposition count {count}")
+                        # print out the full description we have been compiling
+                        # TODO: save incident description somewhere (like an obj or an array)
+                        # incident_description = '' #reset back to empty
+                        desc_cont = False # reset bool back
+                        # remove the 'Disposition: ' prefix and any \n or . chars
+                        disposition = line.split('Disposition: ')[1].strip().replace('.', '')
+                        print('Disposition: ' + disposition)
 
-                else:
-                    print(" [*] Not an extended description")
-                    initiator_location = line.split(" at ")
-                    initiator_location[1] = initiator_location[1].strip(".\n")
-                    print(initiator_location)
-                    desc_cont = True
+                        # Format the data for writing
+                        # Separating the date from the ReferenceNum
+                        act_date = time_type_date[2]
+                        act_date = act_date[:6] # First 6 characters are the date in YYMMDD
+                        # Add underscores to separate, PDAP hates hyphens (idk why)
+                        activity_date = '_'.join([act_date[:2], act_date[2:4], act_date[4:6]])
+                        # Adds 20 to the beginning to match MySQL standard of yyyy-mm-dd
+                        activity_date = "20" + activity_date
+                        reference_num = time_type_date[2]
 
-            count += 1
+                        place_street_city = initiator_location[1].split(",")
+                        if len(place_street_city) != 3:
+                            num_add = 3 - len(place_street_city)
+                            for i in range(num_add):
+                                place_street_city.append("null")
 
-            # if count % 2 == 0:
-            #     initiator line.split("at")
+                    
+                        #  ReferenceNum, ActivityDate, ActivityTime, ActivityType, ActivityInitiator, ActivityDescription, Disposition, ActivityPlace, ActivityStreet, ActivityCity
+                        if was_desc_cont:
+                            all_data = [reference_num, activity_date, time_type_date[0], time_type_date[1], initiator_location[0], incident_description, disposition, place_street_city[0], place_street_city[1], place_street_city[2]]
+                        else:
+                            all_data = [reference_num, activity_date, time_type_date[0], time_type_date[1], initiator_location[0], "null", disposition, place_street_city[0], place_street_city[1], place_street_city[2]]
+                        formated_data.write(str(all_data) + " \n")
+                        was_desc_cont = False # resset bool back
+
+                        print(all_data)
+                        print('======================') # pretty print a closing line saying we are done
+                        count = 0 # reset back, next line will be a header
+
+                    # if true, then a prior line set this so we know to add it to the description
+                    elif desc_cont:
+                        print(f"desc_cont {count}" )
+                        print("  [!] There is a description")
+                        print("LINE: " + line)
+                        incident_description = line.strip(".\n")
+                        print("incident_description desc_cont: " + incident_description)
+                        was_desc_cont = True
+                    # if disposition is not in the line, then treat it as the usual initiator and location line
+                    else:
+                        print(f"else {count}")
+                        initiator_location = line.split(" at ")
+                        initiator_location[1] = initiator_location[1].strip(".\n")
+                        print(initiator_location)
+                        # set desc_cont to True, will remain true unless `Disposition` is found in line
+                        desc_cont = True
+                count += 1
+
+                # if count % 2 == 0:
+                #     initiator line.split("at")
