622e48888288a34be07a960c7eeb432fb366f417
Stabs <comportfailure@gmail.com>
2021-04-26 10:08:03 +0000

    Add template crimegraphics bulletin

--- Base_Scripts/Scrapers/crimegraphics/configs.py
+++ Base_Scripts/Scrapers/crimegraphics/configs.py
@@ -1,5 +1,6 @@
 url = "https://hsupd.crimegraphics.com/2013/default.aspx"
 department_code = "HSUPD"
+# Only needed for crimegraphics_clery
 list_header = [
     "ChargeDescription",
     "CaseNum",
