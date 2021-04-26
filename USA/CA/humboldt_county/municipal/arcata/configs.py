c4857be02f0979f291f3c9970c2f6ff720ab0b95
Stabs <comportfailure@gmail.com>
2021-04-26 10:12:41 +0000

    Arcata scraper added

---
+++ USA/CA/humboldt_county/municipal/arcata/configs.py
@@ -0,0 +1,11 @@
+url = "https://arcata.crimegraphics.com/2013/default.aspx"
+department_code = "arcata"
+# Only needed for crimegraphics_clery
+list_header = [
+    "ChargeDescription",
+    "CaseNum",
+    "ReportDate",
+    "OffenseDate",
+    "Location",
+    "ChargeDisposition",
+]
