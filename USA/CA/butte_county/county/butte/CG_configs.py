13ebc1b6b8cab6507d1a1ca3148d4c9d246d72b1
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2021-04-26 19:22:07 +0000

    Add Butte County Sheriff CG bulletin scraper (#85)
    * Working on crimegraphic's bulletin scraper
    
    * Working on crimegraphic's bulletin scraper
    
    * Just pushing for later
    
    * Headache fuel
    
    * It works, as of now
    
    * Won't need these lines
    
    * Pushing for Eric to take a look
    
    * need pandas
    
    * Parse test refactor (#29)
    
    * Solution for Parsing Daily Bulletin
    
    Logic to determine if the line is a description or a disposition.
    
    * added incident_description var
    
    * update comment
    
    Co-authored-by: Eric Turner <turner.eric7@gmail.com>
    
    * Remove unneeded space
    
    * Now stores lists within a master list, and converts to csv when done
    
    * Just pushing to access later
    
    * saving
    
    * Add crimegraphics bulleting scraper, and it's data parser
    
    * Moved testing out of USA
    
    * Add template crimegraphics bulletin
    
    * Arcata scraper added
    
    * Update name in the inits
    
    * Update for rename
    
    * Arcata scraper added
    
    * Update for rename
    
    * Update for rename
    
    * Update for rename
    
    * Update for rename
    
    * Arcata scraper added
    
    * Update data_parser to handle different formats
    
    * Rename Crimegraphics' config file to CG_configs to prevent confusion
    
    * Working on crimegraphic's bulletin scraper
    
    * Just pushing for later
    
    * Headache fuel
    
    * Parse test refactor (#29)
    
    * Solution for Parsing Daily Bulletin
    
    Logic to determine if the line is a description or a disposition.
    
    * added incident_description var
    
    * update comment
    
    Co-authored-by: Eric Turner <turner.eric7@gmail.com>
    
    * Remove unneeded space
    
    * Just pushing to access later
    
    * saving
    
    * It works, as of now
    
    * Won't need these lines
    
    * Pushing for Eric to take a look
    
    * need pandas
    
    * Now stores lists within a master list, and converts to csv when done
    
    * Add crimegraphics bulleting scraper, and it's data parser
    
    * Moved testing out of USA
    
    * Add template crimegraphics bulletin
    
    * Update name in the inits
    
    * Update for rename
    
    * Update for rename
    
    * Update for rename
    
    * Update for rename
    
    * Arcata scraper added
    
    * Update data_parser to handle different formats
    
    * Rename Crimegraphics' config file to CG_configs to prevent confusion
    
    * Butte county bulletin scraper
    
    * Declare inc_description before loop
    
    * Code works, ignore failing build
    
    * Butte county bulletin scraper
    
    * Declare inc_description before loop
    
    Co-authored-by: Eric Turner <turner.eric7@gmail.com>

---
+++ USA/CA/butte_county/county/butte/CG_configs.py
@@ -0,0 +1,11 @@
+url = "https://bcso.crimegraphics.com/2013/default.aspx"
+department_code = "BCSO"
+# Only needed for crimegraphics_clery
+list_header = [
+    "ChargeDescription",
+    "CaseNum",
+    "ReportDate",
+    "OffenseDate",
+    "Location",
+    "ChargeDisposition",
+]
