89687c554a6db2d565099b373eb4201cc9a48e50
Stabs <comportfailure@gmail.com>
2021-04-26 10:14:39 +0000

    Update name in the inits

--- common/base_scrapers/__init__.py
+++ common/base_scrapers/__init__.py
@@ -1,6 +1,6 @@
 from .list_pdf_scrapers import list_pdf_v2
 from .list_pdf_scrapers import list_pdf_v3
 from .list_pdf_scrapers import single_pdf_scraper
-from .crimegraphics import crimegraphics_scraper
+from .crimegraphics import crimegraphics_clery
 from .crimegraphics import crimegraphics_arrest
 from .crimegraphics import crimegraphics_bulletin
