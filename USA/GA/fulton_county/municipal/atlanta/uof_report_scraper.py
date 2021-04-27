f376e02551431f2264e3fc13d413968df92e4e8a
Stabs <comportfailure@gmail.com>
2021-04-27 11:33:33 +0000

    Add use of force scraper to Atlanta GA

---
+++ USA/GA/fulton_county/municipal/atlanta/uof_report_scraper.py
@@ -0,0 +1,12 @@
+import sys
+import os
+from configs import uof_configs as configs
+from pathlib import Path
+
+p = Path(__file__).resolve().parents[5]
+sys.path.insert(1, str(p))
+from common import list_pdf_v2
+
+save_dir = "./data/"
+
+list_pdf_v2(configs, save_dir, name_in_url=False)
