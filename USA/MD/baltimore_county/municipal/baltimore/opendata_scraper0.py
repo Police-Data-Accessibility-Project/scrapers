1d02d117cd06cc555ae107c94ae8fd1abfe10111
CaptainStabs <comportfailure@gmail.com>
2021-05-09 15:51:08 -0400

    Added status_code check

--- USA/MD/baltimore_county/municipal/baltimore/opendata_scraper0.py
+++ USA/MD/baltimore_county/municipal/baltimore/opendata_scraper0.py
@@ -33,20 +33,21 @@ def opendata_scraper(url_table, save_table, save_folder):
     for i, row in enumerate(url_table):
         # get the api response
         response = requests.get(url_table[i])
+        if response.status_code == 200:
+            # this could be achieved by using the "Return Count Only" option when generating the query (later on)
+            updated = page_update(response, save_folder + save_table[i], loop=True)
+            # print("Update bool: " + str(updated))
 
-        # this could be achieved by using the "Return Count Only" option when generating the query (later on)
-        updated = page_update(response, save_folder + save_table[i], loop=True)
-        # print("Update bool: " + str(updated))
+            if updated:
+                parsed = json.loads(response.text)
+                date_name = date.today()
+                file_name = (
+                    str(date_name).replace("-", "_") + "_" + save_table[i].strip("/")
+                )
 
-        if updated:
-            parsed = json.loads(response.text)
-            date_name = date.today()
-            file_name = (
-                str(date_name).replace("-", "_") + "_" + save_table[i].strip("/")
-            )
-
-            with open(save_folder + save_table[i] + file_name + ".json", "w") as output:
-                output.write(json.dumps(parsed, indent=4, sort_keys=False))
-
+                with open(save_folder + save_table[i] + file_name + ".json", "w") as output:
+                    output.write(json.dumps(parsed, indent=4, sort_keys=False))
+        else:
+            print(f" [!!!] Url {url_table[i]} returned code {response.status_code}. Check that the url is correct." )
 
 opendata_scraper(url_table, save_table, save_folder)
