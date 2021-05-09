367a519b263805db9f57d35f6a62151bbbb9172b
CaptainStabs <comportfailure@gmail.com>
2021-05-09 15:45:47 -0400

    Turned the opendata_scraper into a function and formatted it

--- USA/MD/baltimore_county/municipal/baltimore/opendata_scraper0.py
+++ USA/MD/baltimore_county/municipal/baltimore/opendata_scraper0.py
@@ -10,23 +10,43 @@ sys.path.insert(1, str(p))
 
 from common.utils import page_update
 
-url_table = ["https://egis.baltimorecity.gov/egis/rest/services/GeoSpatialized_Tables/Part1_Crime/FeatureServer/0/query?where=1%3D1&outFields=CrimeDateTime,CrimeCode,Location,Description,Inside_Outside,Weapon,Post,District,Neighborhood,GeoLocation,Premise,VRIName,Total_Incidents&returnGeometry=false&outSR=4326&f=json", "https://egis.baltimorecity.gov/egis/rest/services/GeoSpatialized_Tables/Arrest/FeatureServer/0/query?where=1%3D1&outFields=ArrestNumber,Age,Gender,Race,ArrestDateTime,ArrestLocation,IncidentOffence,IncidentLocation,Charge,ChargeDescription,District,Post,Neighborhood,GeoLocation&returnGeometry=false&outSR=4326&f=json", "https://opendata.baltimorecity.gov/egis/rest/services/NonSpatialTables/CallsForService_2021_Present/FeatureServer/0/query?where=1%3D1&outFields=*&returnGeometry=false&outSR=4326&f=json", "https://services1.arcgis.com/UWYHeuuJISiGmgXx/arcgis/rest/services/911_Calls_For_Service_2018_csv/FeatureServer/0/query?where=1%3D1&outFields=*&returnGeometry=false&outSR=4326&f=json", "https://services1.arcgis.com/UWYHeuuJISiGmgXx/arcgis/rest/services/911_Calls_For_Service_2020_csv/FeatureServer/0/query?where=1%3D1&outFields=*&returnGeometry=false&outSR=4326&f=json", "https://services1.arcgis.com/UWYHeuuJISiGmgXx/arcgis/rest/services/911_Calls_For_Service_2019_csv/FeatureServer/0/query?where=1%3D1&outFields=*&returnGeometry=false&outSR=4326&f=json"]
-save_table = ["part1_crime/", "arrests/", "CFS_2021/", "CFS_2018/", "CFS_2020/", "CFS_2019/"]
-
+url_table = [
+    "https://egis.baltimorecity.gov/egis/rest/services/GeoSpatialized_Tables/Part1_Crime/FeatureServer/0/query?where=1%3D1&outFields=CrimeDateTime,CrimeCode,Location,Description,Inside_Outside,Weapon,Post,District,Neighborhood,GeoLocation,Premise,VRIName,Total_Incidents&returnGeometry=false&outSR=4326&f=json",
+    "https://egis.baltimorecity.gov/egis/rest/services/GeoSpatialized_Tables/Arrest/FeatureServer/0/query?where=1%3D1&outFields=ArrestNumber,Age,Gender,Race,ArrestDateTime,ArrestLocation,IncidentOffence,IncidentLocation,Charge,ChargeDescription,District,Post,Neighborhood,GeoLocation&returnGeometry=false&outSR=4326&f=json",
+    "https://opendata.baltimorecity.gov/egis/rest/services/NonSpatialTables/CallsForService_2021_Present/FeatureServer/0/query?where=1%3D1&outFields=*&returnGeometry=false&outSR=4326&f=json",
+    "https://services1.arcgis.com/UWYHeuuJISiGmgXx/arcgis/rest/services/911_Calls_For_Service_2018_csv/FeatureServer/0/query?where=1%3D1&outFields=*&returnGeometry=false&outSR=4326&f=json",
+    "https://services1.arcgis.com/UWYHeuuJISiGmgXx/arcgis/rest/services/911_Calls_For_Service_2020_csv/FeatureServer/0/query?where=1%3D1&outFields=*&returnGeometry=false&outSR=4326&f=json",
+    "https://services1.arcgis.com/UWYHeuuJISiGmgXx/arcgis/rest/services/911_Calls_For_Service_2019_csv/FeatureServer/0/query?where=1%3D1&outFields=*&returnGeometry=false&outSR=4326&f=json",
+]
+save_table = [
+    "part1_crime/",
+    "arrests/",
+    "CFS_2021/",
+    "CFS_2018/",
+    "CFS_2020/",
+    "CFS_2019/",
+]
 save_folder = "./data/"
 
-for i, row in enumerate(url_table):
 
-    response = requests.get(url_table[i])
+def opendata_scraper(url_table, save_table, save_folder):
+    for i, row in enumerate(url_table):
+        # get the api response
+        response = requests.get(url_table[i])
 
-    # this could be achieved by using the "Return Count Only" option when generating the query
-    updated = page_update(response, save_folder+save_table[i], loop=True)
-    print("Update bool: " + str(updated))
+        # this could be achieved by using the "Return Count Only" option when generating the query (later on)
+        updated = page_update(response, save_folder + save_table[i], loop=True)
+        # print("Update bool: " + str(updated))
 
-    if updated:
-        parsed = json.loads(response.text)
-        date_name = date.today()
-        file_name = str(date_name).replace("-", "_") + "_" + save_table[i].strip("/")
+        if updated:
+            parsed = json.loads(response.text)
+            date_name = date.today()
+            file_name = (
+                str(date_name).replace("-", "_") + "_" + save_table[i].strip("/")
+            )
 
-        with open(save_folder + save_table[i] + file_name + ".json","w") as output:
-            output.write(json.dumps(parsed, indent=4, sort_keys=False))
+            with open(save_folder + save_table[i] + file_name + ".json", "w") as output:
+                output.write(json.dumps(parsed, indent=4, sort_keys=False))
+
+
+opendata_scraper(url_table, save_table, save_folder)
