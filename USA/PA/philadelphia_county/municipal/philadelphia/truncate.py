2324699e268e5a950b1fe1ba74827554e361e7cb
Stabs <comportfailure@gmail.com>
2021-05-11 11:06:39 +0000

    truncate tool

---
+++ USA/PA/philadelphia_county/municipal/philadelphia/truncate.py
@@ -0,0 +1,5 @@
+filename = "/home/kali/github/PDAP-Scrapers/USA/PA/philadelphia_county/municipal/philadelphia/data/vehicle_ped_investigations/2018-2019/2021_05_11_2018-2019.csv"
+f = open(filename, "a")
+f.truncate(930)
+f.write("\nTRUNCATED")
+f.close()
