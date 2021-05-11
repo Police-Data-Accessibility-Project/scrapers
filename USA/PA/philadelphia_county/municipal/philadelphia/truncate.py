036d8eac37ee010a6774f669540a3e8fcfa45ed9
Stabs <comportfailure@gmail.com>
2021-05-11 12:25:10 +0000

    Update truncate tool to loop

--- USA/PA/philadelphia_county/municipal/philadelphia/truncate.py
+++ USA/PA/philadelphia_county/municipal/philadelphia/truncate.py
@@ -1,20 +1,17 @@
 import os
 
-def list_files(dir):
-    r = []
-    for root, dirs, files in os.walk(dir):
-        for name in files:
-            r.append(os.path.join(root, name))
-    return r
-
-filename = "./data/vehicle_ped_investigations/2020-present/2021_05_11_2020-present.csv"
-
+# filename = "./data/vehicle_ped_investigations/2020-present/2021_05_11_2020-present.csv"
+dir  = "./"
 for root, dirs, files in os.walk(dir):
-    if ".csv" in files:
-        filename = root + files
-        f = open(filename, "a")
-        read_file = f.read()
-        if "TRUNCATED" not in read_file:
-            f.truncate(930)
-            f.write("\nTRUNCATED")
-            f.close()
+    print(files)
+    # print(files)
+    for i in range(len(files)):
+        if files[i].endswith(".csv"):
+            filename = f"{root}/{files[i]}"
+            print(filename)
+            f = open(filename, "a+")
+            read_file = f.read()
+            if "TRUNCATED" not in read_file:
+                f.truncate(930)
+                f.write("\nTRUNCATED")
+                f.close()
