2605f7c99081c9d1950a4f70f67f7cabd559bbbc
Stabs <comportfailure@gmail.com>
2021-05-11 11:34:25 +0000

    update tuncate

--- USA/PA/philadelphia_county/municipal/philadelphia/truncate.py
+++ USA/PA/philadelphia_county/municipal/philadelphia/truncate.py
@@ -1,5 +1,20 @@
-filename = "/home/kali/github/PDAP-Scrapers/USA/PA/philadelphia_county/municipal/philadelphia/data/vehicle_ped_investigations/2018-2019/2021_05_11_2018-2019.csv"
-f = open(filename, "a")
-f.truncate(930)
-f.write("\nTRUNCATED")
-f.close()
+import os
+
+def list_files(dir):
+    r = []
+    for root, dirs, files in os.walk(dir):
+        for name in files:
+            r.append(os.path.join(root, name))
+    return r
+
+filename = "./data/vehicle_ped_investigations/2020-present/2021_05_11_2020-present.csv"
+
+for root, dirs, files in os.walk(dir):
+    if ".csv" in files:
+        filename = root + files
+        f = open(filename, "a")
+        read_file = f.read()
+        if "TRUNCATED" not in read_file:
+            f.truncate(930)
+            f.write("\nTRUNCATED")
+            f.close()
