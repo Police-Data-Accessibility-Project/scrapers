7abbf8ed0d2a317abd02bba22d20cc4c8ed7d8ff
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2021-05-12 11:39:49 -0400

    Tool for extracting fields created

---
+++ USA/OH/hamilton_county/municipal/cincinnati/field_extractor.py
@@ -0,0 +1,25 @@
+import os
+
+# filename = "./data/vehicle_ped_investigations/2020-present/2021_05_11_2020-present.csv"
+dir = "./"
+fields = []
+for root, dirs, files in os.walk(dir):
+
+    # print(files)
+    # print(files)
+    for i in range(len(files)):
+        if files[i].endswith(".csv"):
+            filename = f"{root}/{files[i]}"
+            # print(filename)
+            f = open(filename, "r")
+            first_line = f.readline()
+            first_line_list = first_line.split(",")
+            for i in range(len(first_line_list)):
+                if first_line_list[i] not in fields:
+                    # print(first_line_list[i])
+                    fields.append(first_line_list[i])
+
+
+with open("fields.txt", "a") as save_fields:
+    for i in range(len(fields)):
+        save_fields.write(f"* {fields[i]}\n")
