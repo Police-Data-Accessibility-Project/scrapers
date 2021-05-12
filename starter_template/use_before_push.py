c1587c818697c60ed2a319562dd33181299220ce
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2021-05-12 11:58:45 -0400

    Fix import

--- starter_template/use_before_push.py
+++ starter_template/use_before_push.py
@@ -3,9 +3,9 @@ import os
 from pathlib import Path
 
 p = Path(__file__).resolve().parents[5]
-sys.path.insert(1, str(p) + "/common/")
+sys.path.insert(1, str(p))
 
-from utils.demo_utils import data_truncater, field_extractor
+from common.utils.demo_utils import data_truncater, field_extractor
 
 dir = "./"
 
