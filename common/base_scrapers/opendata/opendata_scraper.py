72341f362a5f6abc54f746b2097972693542276c
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2021-05-12 12:05:31 -0400

    Dallas tx opendata scraper (#98)
    * Add opendata scraper to template folder
    
    * Updated by script
    
    * Create dallas opendata scraper
    
    * Added content type check
    
    * Created by script
    
    * Create and update readme

--- common/base_scrapers/opendata/opendata_scraper.py
+++ common/base_scrapers/opendata/opendata_scraper.py
@@ -15,6 +15,7 @@ def opendata_scraper(url_table, save_table, save_folder):
     for i, row in enumerate(url_table):
         # get the api response
         response = requests.get(url_table[i])
+        content_type = response.headers["content-type"]
         if response.status_code == 200:
             # this could be achieved by using the "Return Count Only" option when generating the query instead of hashing the entire response (later on)
             updated = page_update(response, save_folder + save_table[i], loop=True, print_output=False)
@@ -22,16 +23,22 @@ def opendata_scraper(url_table, save_table, save_folder):
 
             if updated:
                 print(f"  [*] Url in index {i} of url_table has updated.")
-                parsed = json.loads(response.text)
+                if "json" in content_type:
+                    parsed = json.loads(response.text)
+
                 date_name = date.today()
                 file_name = (
                     str(date_name).replace("-", "_") + "_" + save_table[i].strip("/")
                 )
-
-                with open(
-                    save_folder + save_table[i] + file_name + ".json", "w"
-                ) as output:
-                    output.write(json.dumps(parsed, indent=4, sort_keys=False))
+                if "json" in content_type:
+                    with open(
+                        save_folder + save_table[i] + file_name + ".json", "w"
+                    ) as output:
+                        output.write(json.dumps(parsed, indent=4, sort_keys=False))
+                        
+                elif "csv" in content_type:
+                    with open(save_folder + save_table[i] + file_name +".csv", "w") as output:
+                        output.write(response.text)
             else:
                 print(f"  [*] Url in index {i} of url_table has not updated.")
         else:
