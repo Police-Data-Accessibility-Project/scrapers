d37fcfd9b93bcfbf564933f6d99de50b2ad4e66e
Stabs <comportfailure@gmail.com>
2021-05-13 12:29:46 +0000

    rename to historical opendata

--- USA/TX/travis_county/municipal/austin/historical_opendata.py
+++ USA/TX/travis_county/municipal/austin/historical_opendata.py
@@ -9,16 +9,16 @@ from common import opendata_scraper
 
 # Change to what you need (remove what you don't)
 url_table = [
-    "URL1",
+    "https://data.austintexas.gov/resource/spbg-9v94.csv",
     "URL2",
 ]
 
 # Change to what you need (remove what you don't)
 save_table = [
-    "URL1_name/",
+    "annual_crime/2015/",
     "arrests/",
 ]
 save_folder = "./data/"
 
 # Optional argument `save_subfolder` allows saving in a subfolder
-opendata_scraper(url_table, save_table, save_folder)
+opendata_scraper(url_table, save_table, save_folder, save_subfolder=True)
