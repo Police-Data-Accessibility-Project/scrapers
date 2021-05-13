7b9743b8e4b6f13f1420869e7c53f1bf1c5d7a73
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2021-05-13 09:45:16 -0400

    generated by scrapers

---
+++ USA/CA/alameda_county/municipality/berkely/historical_opendata.py
@@ -0,0 +1,20 @@
+import os
+import sys
+from pathlib import Path
+
+p = Path(__file__).resolve().parents[5]
+sys.path.insert(1, str(p))
+
+from common import opendata_scraper
+
+# Change to what you need (remove what you don't)
+url_table = [
+    "https://data.cityofberkeley.info/resource/4tbf-3yt8.csv",
+    "https://data.cityofberkeley.info/resource/efkp-2py4.csv",
+]
+
+# Change to what you need (remove what you don't)
+save_table = ["stop_data/historical/", "annual_part1/"]
+save_folder = "./data/"
+
+opendata_scraper(url_table, save_table, save_folder, save_subfolder=True)
