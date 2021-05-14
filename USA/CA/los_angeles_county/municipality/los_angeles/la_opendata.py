4f821505f0c29c6ca2227bc795fcef1f51cc3ec5
Stabs <comportfailure@gmail.com>
2021-05-14 15:18:15 +0000

    pep8 archived

--- USA/CA/los_angeles_county/municipality/los_angeles/la_opendata.py
+++ USA/CA/los_angeles_county/municipality/los_angeles/la_opendata.py
@@ -9,12 +9,16 @@ from common import opendata_scraper2
 
 save_url = [
     ["crime_data/2020-present/", "https://data.lacity.org/resource/2nrs-mtv8.csv"],
-    ["vehicle_ped_stop/2010-present/", "https://data.lacity.org/resource/ci25-wgt7.csv"],
+    [
+        "vehicle_ped_stop/2010-present/",
+        "https://data.lacity.org/resource/ci25-wgt7.csv",
+    ],
     ["arrests/2020-present/", "https://data.lacity.org/resource/amvf-fr72.csv"],
     ["response_metrics/citywide/", "https://data.lacity.org/resource/kcsj-s69p.csv"],
-    ["cfs/2021/", "https://data.lacity.org/resource/cibt-wiru.csv"]
+    ["cfs/2021/", "https://data.lacity.org/resource/cibt-wiru.csv"],
 ]
-"save_folder" = "./data/"
+
+save_folder = "./data/"
 
 # Optional argument `save_subfolder` allows saving in a subfolder
 # Optional argument `sleep_time` should be set to the site's crawl-delay,
