9e5e85042cf6f5894331cc3bc07b7c031d7faaf9
Stabs <comportfailure@gmail.com>
2021-05-14 12:20:22 +0000

    update NY to use opendata_scraper2

--- USA/NY/municipal/new_york/new_york_opendata.py
+++ USA/NY/municipal/new_york/new_york_opendata.py
@@ -5,21 +5,19 @@ from pathlib import Path
 p = Path(__file__).resolve().parents[4]
 sys.path.insert(1, str(p))
 
-from common import opendata_scraper
+from common import opendata_scraper2
 
 # Change to what you need (remove what you don't)
-url_table = [
-    "https://data.cityofnewyork.us/api/views/5uac-w243/rows.csv?accessType=DOWNLOAD",
-    "https://data.cityofnewyork.us/api/views/57mv-nv28/rows.csv?accessType=DOWNLOAD",
-]
-
-# Change to what you need (remove what you don't)
-save_table = [
-    "crime_data/",  # called "complaints" on website
-    "crime_data/historic",
+save_url = [
+    [
+        "crime_data/",
+        "https://data.cityofnewyork.us/api/views/5uac-w243/rows.csv?accessType=DOWNLOAD",
+    ],
+    [
+        "crime_data/historic",
+        "https://data.cityofnewyork.us/api/views/57mv-nv28/rows.csv?accessType=DOWNLOAD",
+    ],
 ]
 save_folder = "./data/"
 
-opendata_scraper(
-    url_table, save_table, save_folder, save_subfolder=True, dictionary=False
-)
+opendata_scraper2(save_url, save_folder, save_subfolder=True)
