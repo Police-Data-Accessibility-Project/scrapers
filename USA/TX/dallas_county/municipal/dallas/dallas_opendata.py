47a7d352d62b96fc87bd443ca85d4ef9b06ce732
Stabs <comportfailure@gmail.com>
2021-05-14 10:22:26 +0000

    Update Dallas Texas scraper to use updated version (as a test)

--- USA/TX/dallas_county/municipal/dallas/dallas_opendata.py
+++ USA/TX/dallas_county/municipal/dallas/dallas_opendata.py
@@ -5,23 +5,20 @@ from pathlib import Path
 p = Path(__file__).resolve().parents[5]
 sys.path.insert(1, str(p))
 
-from common import opendata_scraper
+from common import opendata_scraper2
 
 # Change to what you need (remove what you don't)
-url_table = [
-    "https://www.dallasopendata.com/resource/4gmt-jyx2.csv",
-    "https://www.dallasopendata.com/resource/qv6i-rri7.csv",
-    "https://www.dallasopendata.com/resource/9fxf-t2tr.csv",
-    "https://www.dallasopendata.com/resource/sdr7-6v3j.csv",
+
+url_save = [
+    [
+        "officer_involved_shootings/",
+        "https://www.dallasopendata.com/resource/4gmt-jyx2.csv",
+    ],
+    ["police_incidents/", "https://www.dallasopendata.com/resource/qv6i-rri7.csv"],
+    ["active_calls/", "https://www.dallasopendata.com/resource/9fxf-t2tr.csv"],
+    ["arrests/", "https://www.dallasopendata.com/resource/sdr7-6v3j.csv"],
 ]
 
-# Change to what you need (remove what you don't)
-save_table = [
-    "officer_involved_shootings/",
-    "police_incidents/",
-    "active_calls/",
-    "arrests/",
-]
 save_folder = "./data/"
 
-opendata_scraper(url_table, save_table, save_folder, dictionary=False)
+opendata_scraper2(url_save, save_folder)
