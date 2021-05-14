6c6945f0b5834e068f3c8cf98659437a380038d6
Stabs <comportfailure@gmail.com>
2021-05-14 09:52:32 +0000

    Update init files

--- common/base_scrapers/__init__.py
+++ common/base_scrapers/__init__.py
@@ -5,3 +5,4 @@ from .crimegraphics import crimegraphics_clery
 from .crimegraphics import crimegraphics_arrest
 from .crimegraphics import crimegraphics_bulletin
 from .opendata import opendata_scraper
+from .opendata import opendata_scraper2
--- common/base_scrapers/opendata/__init__.py
+++ common/base_scrapers/opendata/__init__.py
@@ -1,1 +1,2 @@
 from .opendata_scraper import opendata_scraper
+from .opendata_scraper_2 import opendata_scraper2
