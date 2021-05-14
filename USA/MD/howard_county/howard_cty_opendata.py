ac9b94053f6fabee48cb09b7134ad973b0457c52
Stabs <comportfailure@gmail.com>
2021-05-14 12:23:20 +0000

    update to use opendata_scraper2

--- USA/MD/howard_county/howard_cty_opendata.py
+++ USA/MD/howard_county/howard_cty_opendata.py
@@ -5,17 +5,15 @@ from pathlib import Path
 p = Path(__file__).resolve().parents[3]
 sys.path.insert(1, str(p))
 
-from common import opendata_scraper
+from common import opendata_scraper2
 
 # Change to what you need (remove what you don't)
-url_table = [
-    "https://opendata.howardcountymd.gov/resource/aas5-u28t.csv",
-    "https://opendata.howardcountymd.gov/resource/qccx-65fg.csv",
-    "https://opendata.howardcountymd.gov/resource/hrwk-c83k.csv",
+save_url = [
+    ["uof_stats/", "https://opendata.howardcountymd.gov/resource/aas5-u28t.csv"],
+    ["cfs/", "https://opendata.howardcountymd.gov/resource/qccx-65fg.csv"],
+    ["crime_by_type/", "https://opendata.howardcountymd.gov/resource/hrwk-c83k.csv"],
 ]
 
-# Change to what you need (remove what you don't)
-save_table = ["uof_stats/", "cfs/", "crime_by_type/"]
 save_folder = "./data/"
 
-opendata_scraper(url_table, save_table, save_folder, dictionary=False)
+opendata_scraper2(save_url, save_folder)
