834e5907df1e94b17728721826e288bd43be7ee1
Stabs <comportfailure@gmail.com>
2021-05-17 09:46:41 +0000

    change from `is_socrata` to `socrata` per 51c16a

--- USA/IL/cook_county/municipal/chicago/chicago_opendata.py
+++ USA/IL/cook_county/municipal/chicago/chicago_opendata.py
@@ -18,4 +18,4 @@ save_folder = "./data/"
 # Optional argument `save_subfolder` allows saving in a subfolder
 # Optional argument `sleep_time` should be set to the site's crawl-delay,
 # which can be found in their robots.txt file_name, default time is 1
-opendata_scraper2(save_url, save_folder, sleep_time=1, is_socrata=True)
+opendata_scraper2(save_url, save_folder, sleep_time=1, socrata=True)
