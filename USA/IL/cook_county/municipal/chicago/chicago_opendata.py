f7ea77862e4e1b7f02c6b9ad8f80c795559ccbc6
Stabs <comportfailure@gmail.com>
2021-05-17 09:40:43 +0000

    rename parameter `is_socrata` to `socrata`

--- USA/IL/cook_county/municipal/chicago/chicago_opendata.py
+++ USA/IL/cook_county/municipal/chicago/chicago_opendata.py
@@ -8,14 +8,14 @@ sys.path.insert(1, str(p))
 from common import opendata_scraper2
 
 save_url = [
-    ["save_folder", "url"],
+    ["foia_log/", "https://data.cityofchicago.org/resource/wjkc-agnm.csv"],
     ["save_folder", "url"],
     ["save_folder", "url"],
     ["save_folder", "url"],
 ]
-"save_folder" = "./data/"
+save_folder = "./data/"
 
 # Optional argument `save_subfolder` allows saving in a subfolder
 # Optional argument `sleep_time` should be set to the site's crawl-delay,
 # which can be found in their robots.txt file_name, default time is 1
-opendata_scraper2(save_url, save_folder, sleep_time=1)
+opendata_scraper2(save_url, save_folder, sleep_time=1, is_socrata=True)
