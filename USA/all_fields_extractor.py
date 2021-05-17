d1eaf78c3680fe4d62d1926ed103669658014d65
Stabs <comportfailure@gmail.com>
2021-05-17 14:09:45 +0000

    Add tool for extracting fields from all folders (csv only)

--- USA/all_fields_extractor.py
+++ USA/all_fields_extractor.py
@@ -2,12 +2,11 @@ import sys
 import os
 from pathlib import Path
 
-p = Path(__file__).resolve().parents[5]
+p = Path(__file__).resolve().parents[1]
 sys.path.insert(1, str(p))
 
 from common.utils.demo_utils import data_truncater, field_extractor
 
 directory = "./"
 
-data_truncater(directory)
 field_extractor(directory)
