e5129d0195a574c03d64eafbf61f9f4e151a97cc
CaptainStabs <comportfailure@gmail.com>
2021-07-01 23:54:28 -0400

    apply partial pep8 formatting

--- scraper_setup.py
+++ scraper_setup.py
@@ -7,11 +7,13 @@ from shutil import copyfile
 ui_file = "common/gui/scraper_ui.ui"
 error_modal = "common/gui/error_modal.ui"
 
+
 class ErrorDialog(QtWidgets.QDialog):
     def __init__(self, parent=None):
         super(ErrorDialog, self).__init__()
         uic.loadUi(error_modal, self)
         # self.show()
+
 
 class ScraperGui(QtWidgets.QMainWindow):
     # is_v3 = False
@@ -19,7 +21,7 @@ class ScraperGui(QtWidgets.QMainWindow):
         super(ScraperGui, self).__init__()
         uic.loadUi(ui_file, self)
 
-        self.tabWidget.setCurrentIndex(0) # Start on the first page
+        self.tabWidget.setCurrentIndex(0)  # Start on the first page
         self.choose_scraper_button.clicked.connect(self.choose_scraper_pressed)
         self.create_scraper_button.clicked.connect(self.create_button_pressed)
         self.show()
@@ -42,27 +44,26 @@ class ScraperGui(QtWidgets.QMainWindow):
 
         # Copy the scraper file
         scraper_name_input = self.scraper_name_input.text()
-        scraper_name = scraper_name_input.replace(" ","_") + "_scraper.py"
+        scraper_name = scraper_name_input.replace(" ", "_") + "_scraper.py"
         template_folder = "./Base_Scripts/Scrapers/list_pdf_extractors/"
-        scraper_input = self.scraper_input.currentText() # Get the selected scraper text
+        scraper_input = self.scraper_input.currentText()  # Get the selected scraper text
         global full_path
         full_path = scraper_save_dir + scraper_name
         # Copy and rename the scraper
         copyfile(template_folder + scraper_input, full_path)
 
         # Edit the save_dir
-        save_dir_input = self.save_dir_input.text().replace(" ","_").rstrip("/") # Clean input of spaces
-        save_dir_input = "save_dir = ./data/" + save_dir_input.replace("./data/","") + "/" # Remove any accidental data prepends
+        save_dir_input = self.save_dir_input.text().replace(" ", "_").rstrip("/")  # Clean input of spaces
+        save_dir_input = "save_dir = ./data/" + save_dir_input.replace("./data/", "") + "/"  # Remove any accidental data prepends
         print(save_dir_input)
         # make sure that black formatting does not affect this
         # outer two quotes should be single
-        default_save_dir = 'save_dir = \"./data/\"'
+        default_save_dir = 'save_dir = "./data/"'
         for line in fileinput.input(full_path, inplace=1):
 
             if default_save_dir in line:
-                line = line.replace(default_save_dir,save_dir_input)
+                line = line.replace(default_save_dir, save_dir_input)
             sys.stdout.write(line)
-
 
     def create_button_pressed(self):
         is_v3 = False
@@ -74,13 +75,12 @@ class ScraperGui(QtWidgets.QMainWindow):
         # print(domain_included_input)
         domain_input = self.domain_input.text()
         sleep_time_input = self.sleep_time_input.value()
-        unimportant_input = self.unimportant_input.toPlainText().rstrip(", ").replace(",", ", ") # Get and clean input
+        unimportant_input = self.unimportant_input.toPlainText().rstrip(", ").replace(",", ", ")  # Get and clean input
         unimportant_input_list = unimportant_input.split(", ")
         # print(unimportant_input_list)
 
         # Get the index of config = {
         try:
-            # print("stuff")
             with open(full_path, "r+") as output:
                 i = 0
                 f = 0
@@ -100,13 +100,26 @@ class ScraperGui(QtWidgets.QMainWindow):
 
             with open(full_path, "r+") as output:
                 # output.seek(config_start)
-                lines = output.readlines()[config_start:] # This doesn't seem to do what I want
+                lines = output.readlines()[config_start:]  # This doesn't seem to do what I want
                 print("Lines length: " + str(len(lines)))
                 # for i in range(config_start, config_end):
                 if not is_v3:
-                    config_list = [f'"webpage":"{webpage_input}"',f'"web_path":"{web_path_input}"', f'"domain_input":"{domain_included_input}"',f'"domain":"{domain_included_input}"',f'"sleep_time":"{sleep_time_input}"']
+                    config_list = [
+                        f'"webpage":"{webpage_input}"',
+                        f'"web_path":"{web_path_input}"',
+                        f'"domain_input":"{domain_included_input}"',
+                        f'"domain":"{domain_included_input}"',
+                        f'"sleep_time":"{sleep_time_input}"',
+                    ]
                 else:
-                    config_list = [f'"webpage":"{webpage_input}"',f'"web_path":"{web_path_input}"',f'"domain_input":"{domain_included_input}"',f'"domain":"{domain_included_input}"',f'"sleep_time":"{sleep_time_input}"',f'"{unimportant_input_list}"']
+                    config_list = [
+                        f'"webpage":"{webpage_input}"',
+                        f'"web_path":"{web_path_input}"',
+                        f'"domain_input":"{domain_included_input}"',
+                        f'"domain":"{domain_included_input}"',
+                        f'"sleep_time":"{sleep_time_input}"',
+                        f'"{unimportant_input_list}"',
+                    ]
 
                 for i in range(len(config_list)):
                     # Need a way to set the pointer to the beginning of the config dictionary and
@@ -114,9 +127,6 @@ class ScraperGui(QtWidgets.QMainWindow):
                     output.seek(config_start + i)
                     output.write("    " + config_list[i] + ",\n")
                     print(str(i) + "     " + lines[i])
-
-
-
 
         except NameError as exception:
             print(str(exception))
