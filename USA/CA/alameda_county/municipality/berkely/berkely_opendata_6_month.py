2beea7c7100cce3e39d5d1820cf2840d88a559b4
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2021-12-13 12:09:09 -0500

    spread out the datasets based on update freq

---
+++ USA/CA/alameda_county/municipality/berkely/berkely_opendata_6_month.py
@@ -0,0 +1,18 @@
+import os
+import sys
+from pathlib import Path
+
+p = Path(__file__).resolve().parents[5]
+sys.path.insert(1, str(p))
+
+from common import opendata_scraper
+
+save_url = [
+    [
+        "cfs/",
+        "https://data.cityofberkeley.info/resource/k2nh-s5h5.csv"
+    ]
+]
+save_folder = "./data/"
+
+opendata_scraper(url_table, save_table, save_folder, save_subfolder=True)
