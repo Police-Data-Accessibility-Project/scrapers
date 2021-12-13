5edea3ed3b76c50742696c5150005f2c433e746e
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2021-12-13 12:09:46 -0500

    moved to a different scraper

--- USA/CA/alameda_county/municipality/berkely/historical_opendata.py
+++ USA/CA/alameda_county/municipality/berkely/historical_opendata.py
@@ -9,12 +9,11 @@ from common import opendata_scraper
 
 # Change to what you need (remove what you don't)
 url_table = [
-    "https://data.cityofberkeley.info/resource/4tbf-3yt8.csv",
     "https://data.cityofberkeley.info/resource/efkp-2py4.csv",
 ]
 
 # Change to what you need (remove what you don't)
-save_table = ["stop_data/historical/", "annual_part1/"]
+save_table = ["annual_part1/"]
 save_folder = "./data/"
 
 opendata_scraper(url_table, save_table, save_folder, save_subfolder=True)
