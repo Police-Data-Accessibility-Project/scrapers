e8799380b21aa5a68a778450c0ba2135b451eac8
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2021-12-13 11:18:05 -0500

    update to opendata scraper v2

--- USA/CA/alameda_county/municipality/berkely/berkely_opendata.py
+++ USA/CA/alameda_county/municipality/berkely/berkely_opendata.py
@@ -7,22 +7,27 @@ sys.path.insert(1, str(p))
 
 from common import opendata_scraper
 
-# Change to what you need (remove what you don't)
-url_table = [
-    "https://data.cityofberkeley.info/resource/k2nh-s5h5.csv",
-    "https://data.cityofberkeley.info/resource/xi7q-nji6.csv",
-    "https://data.cityofberkeley.info/resource/7ykt-c32j.csv",
-    "https://data.cityofberkeley.info/resource/ysvs-bcge.csv",
-    "https://data.cityofberkeley.info/api/views/ysvs-bcge/files/d446295a-fe8f-4574-b22f-a8b4d097c977?download=true&filename=Action_Taken.xlsx",
-]
-
-# Change to what you need (remove what you don't)
-save_table = [
-    "cfs/",
-    "arrests/",
-    "jail_bookings/",
-    "stop_data/",
-    "stop_data/action_taken/",
+save_url = [
+    [
+        "cfs/",
+        "https://data.cityofberkeley.info/resource/k2nh-s5h5.csv"
+    ],
+    [
+        "arrests/",
+        "https://data.cityofberkeley.info/resource/xi7q-nji6.csv"
+    ],
+    [
+        "jail_bookings/",
+        "https://data.cityofberkeley.info/resource/7ykt-c32j.csv"
+    ],
+    [
+        "stop_data/",
+        "https://data.cityofberkeley.info/resource/ysvs-bcge.csv"
+    ],
+    [
+        "stop_data/action_taken/",
+        "https://data.cityofberkeley.info/api/views/ysvs-bcge/files/d446295a-fe8f-4574-b22f-a8b4d097c977?download=true&filename=Action_Taken.xlsx"
+    ]
 ]
 save_folder = "./data/"
 
