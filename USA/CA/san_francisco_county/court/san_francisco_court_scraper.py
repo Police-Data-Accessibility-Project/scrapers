a3cafa0ac3892e128338a2f74ebb01830d4d09ba
douglaskrouth <douglaskrouth@gmail.com>
2023-02-19 16:03:16 -0600

    cleaned up date range implementation

--- USA/CA/san_francisco_county/court/san_francisco_court_scraper.py
+++ USA/CA/san_francisco_county/court/san_francisco_court_scraper.py
@@ -22,7 +22,7 @@ class SF_Court_Scraper:
         date: datetime.date("YYYY-MM-DD")
         date_range: tuple of two datetime.date values (start, end)
         ---
-        Example date_range: (datetime.date("2021-02-01"), datetime.date("2021-03-01"))
+        Example date_range: ("2021-01-01", "2021-02-01")
         """
         self.sf_court_entry_link = r"https://webapps.sftc.org/captcha/captcha.dll?referrer=https://webapps.sftc.org/ci/CaseInfo.dll?"
         # Selenium timeout param in seconds. If this is too low we won't be able to complete the CAPTCHA before it times out.
@@ -33,7 +33,11 @@ class SF_Court_Scraper:
         # We also want to have a check in place to see whether a single date was also passed. If this is the case, we'll use the single date value instead of the range and provide log output.
         if date_range and not date:
             # Calculate all of the dates that we want to scrape from the SF Court portal, excluding weekends
-            num_days_scraping_period = (date_range[1] - date_range[0]).days + 1
+
+            num_days_scraping_period = (
+                datetime.strptime(date_range[1], "%Y-%m-%d")
+                - datetime.strptime(date_range[0], "%Y-%m-%d")
+            ).days + 1
             temp = pd.date_range(date_range[0], periods=num_days_scraping_period)
             self.dates_to_scrape = temp.format(
                 formatter=lambda x: x.strftime("%Y-%m-%d")
@@ -62,7 +66,7 @@ class SF_Court_Scraper:
 
         # We made it through the CAPTCHA!
         new_filings_menu_xpath = driver.find_element_by_id("ui-id-3").click()
-        time.sleep(10)
+        time.sleep(2)
         # End condition with driver, close browser
         if driver.window_handles:
             driver.close()
@@ -71,12 +75,7 @@ class SF_Court_Scraper:
 
 
 def main():
-    temp = SF_Court_Scraper(
-        date_range=(
-            datetime.strptime("2021-01-01", "%Y-%m-%d"),
-            datetime.strptime("2021-01-05", "%Y-%m-%d"),
-        )
-    )
+    temp = SF_Court_Scraper(date_range=("2022-01-01", "2022-01-05"))
     print(temp.dates_to_scrape)
 
 
