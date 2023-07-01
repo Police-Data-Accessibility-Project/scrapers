b3db0a4391f63e1034be839715974b9a1db74800
Kylie <github.a5cee@kyscott.aleeas.com>
2023-07-01 16:34:36 -0400

    Output unique state section

--- index.py
+++ index.py
@@ -1,8 +1,5 @@
 import csv
 
-in_repo = []
-not_in_repo = []
-
 
 def get_data():
     with open('PDAP Data Sources.csv', encoding='utf-8-sig') as data:
@@ -21,6 +18,25 @@ def in_repo_filter(row):
 def write_md():
     md = open('scraper_index.md', 'w')
     md.write('# Scraper Index\n\n')
+    md.write('<details>\n')
+    md.write('\t<summary>In this repo</summary>\n')
+
+    current_state = not_in_repo[0]['state']
+    for row in not_in_repo:
+        if current_state != row['state']:
+            md.write('\t</details>\n')
+            write_state_header(md, row)
+            current_state = row['state']
+
+    md.write('</details>\n')
+
+
+    md.close()
+
+
+def write_state_header(md, row):
+    md.write('\t<details>\n')
+    md.write(f'\t\t<summary>{row["state"]}</summary>\n')
 
 
 def main():
@@ -36,4 +52,8 @@ def main():
 
 
 if __name__ == '__main__':
+    in_repo = []
+    not_in_repo = []
+    md = None
+
     main()
