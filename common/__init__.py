242472159eb236cac5a7021c463bb88e4d628b1f
Kylie <github.a5cee@kyscott.aleeas.com>
2023-07-14 21:09:51 -0400

    Fix scraper crash

--- common/__init__.py
+++ common/__init__.py
@@ -5,4 +5,3 @@ from .base_scrapers.list_pdf_scrapers import list_pdf_v2
 from .base_scrapers.list_pdf_scrapers import single_pdf_scraper
 from .base_scrapers.opendata import opendata_scraper
 from .base_scrapers.opendata import opendata_scraper2
-from .etl.main import schema_load
