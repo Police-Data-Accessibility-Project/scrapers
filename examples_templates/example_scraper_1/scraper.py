4adde3597102a6d8e8e27cc1657bb2b2e116cbf6
Kylie <github.a5cee@kyscott.aleeas.com>
2023-08-18 22:30:43 -0400

    Update CONTRIBUTING with example scrapers

--- examples_templates/example_scraper_1/scraper.py
+++ examples_templates/example_scraper_1/scraper.py
@@ -53,4 +53,5 @@ save_dir = "./data/officer_training/"
 file_name = "training-report-hours-total.xls"
 url = "https://web.archive.org/web/20230819011054/https://data.wprdc.org/dataset/8a7d1d09-c8d3-4a0b-9812-11f72bb22103/resource/8c810512-007e-4d10-bfb9-a027a4fd682c/download/training-report-hours-total.xls"
 
-#get_xls(save_dir, file_name, url, sleep_time=15)
+# This last one is needed because the csv for training report hours total from the website was blank xls file is retrieved instead.
+get_xls(save_dir, file_name, url, sleep_time=15)
