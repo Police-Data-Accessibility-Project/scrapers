4e668eceef787339cf3ea333531a222ae8ed4bb7
Kylie <github.a5cee@kyscott.aleeas.com>
2023-08-27 22:07:35 -0400

    Initial commit

---
+++ scrapers_library/data_portals/muckrock/muckrock_scraper.py
@@ -0,0 +1,3 @@
+#https://www.muckrock.com/foi/pittsburgh-130/traffic-stops-140596/#files
+#https://www.muckrock.com/api_v1/foia/?agency=&embargo=unknown&format=json&title=Traffic+Stops
+#https://www.muckrock.com/foi/kingston-30521/roster-and-hire-dates-143168/#files
