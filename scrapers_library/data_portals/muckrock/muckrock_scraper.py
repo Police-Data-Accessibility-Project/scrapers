53e72377f05102af7fa70773cb51f1c0d34dd2f2
Kylie <github.a5cee@kyscott.aleeas.com>
2023-08-30 13:01:36 -0400

    Create template, function outline

--- scrapers_library/data_portals/muckrock/muckrock_scraper.py
+++ scrapers_library/data_portals/muckrock/muckrock_scraper.py
@@ -1,3 +1,25 @@
+import sys
+
+import requests
+from from_root import from_root
+
+p = from_root('CODE_OF_CONDUCT.md').parent
+sys.path.insert(1, str(p))
+
+from utils.file_downloaders.downloaders import get_pdf, get_xls
+
 #https://www.muckrock.com/foi/pittsburgh-130/traffic-stops-140596/#files
 #https://www.muckrock.com/api_v1/foia/?agency=&embargo=unknown&format=json&title=Traffic+Stops
 #https://www.muckrock.com/foi/kingston-30521/roster-and-hire-dates-143168/#files
+#https://www.muckrock.com/agency/pittsburgh-130/pittsburgh-bureau-police-357/
+
+def get_single_file(url):
+    get_xls("./data/", "test.xlsx", url, sleep_time=1)
+
+
+def get_foia_files(url):
+    pass
+
+
+def get_all_agency_files(url):
+    pass
