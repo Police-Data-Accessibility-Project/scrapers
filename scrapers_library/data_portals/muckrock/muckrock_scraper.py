d72a37c68d74b33a002f99ac2c0b3c432318dcba
Kylie <github.a5cee@kyscott.aleeas.com>
2023-09-01 20:29:02 -0400

    Complete get_single_file function

--- scrapers_library/data_portals/muckrock/muckrock_scraper.py
+++ scrapers_library/data_portals/muckrock/muckrock_scraper.py
@@ -1,23 +1,40 @@
+import os
 import sys
 
 import requests
-from from_root import from_root
+#from from_root import from_root
 
-p = from_root('CODE_OF_CONDUCT.md').parent
-sys.path.insert(1, str(p))
-
-from utils.file_downloaders.downloaders import get_pdf, get_xls
+#p = from_root('CODE_OF_CONDUCT.md').parent
+#sys.path.insert(1, str(p))
 
 #https://www.muckrock.com/foi/pittsburgh-130/traffic-stops-140596/#files
-#https://www.muckrock.com/api_v1/foia/?agency=&embargo=unknown&format=json&title=Traffic+Stops
+#https://www.muckrock.com/api_v1/foia/?format=json&title=Traffic+Stops
 #https://www.muckrock.com/foi/kingston-30521/roster-and-hire-dates-143168/#files
 #https://www.muckrock.com/agency/pittsburgh-130/pittsburgh-bureau-police-357/
 
-def get_single_file(url):
-    get_xls("./data/", "test.xlsx", url, sleep_time=1)
+def get_single_file(save_folder, url, file_name=""):
+    if not file_name:
+        i = url.rfind("/")
+        file_name = url[i:]
+
+    file_path = save_folder + file_name
+
+    if os.path.exists(file_path):
+        print(f"File {file_path} already exists.")
+        return
+
+    if not os.path.exists(save_folder):
+       os.makedirs(save_folder)
+
+    print(f"Retrieving file {file_path}...")
+
+    r = requests.get(url, stream=True)
+    with open(file_path, 'wb') as fd:
+        for chunk in r.iter_content():
+            fd.write(chunk)
 
 
-def get_foia_files(url):
+def get_foia_files(save_folder, url):
     pass
 
 
--- scrapers_library/data_portals/muckrock/template/single_file_template.py
+++ scrapers_library/data_portals/muckrock/template/single_file_template.py
@@ -2,11 +2,12 @@ import sys
 
 from from_root import from_root
 
-p = from_root('CODE_OF_CONDUCT.md').parent
+p = from_root('CONTRIBUTING.md').parent
 sys.path.insert(1, str(p))
 
 from scrapers_library.data_portals.muckrock.muckrock_scraper import get_single_file
 
+save_folder = "./data/"
 url = "https://cdn.muckrock.com/foia_files/2023/03/27/TrafficStops_2020-2022_RTK129-01-2023.xlsx"
 
-get_single_file(url)
+get_single_file(save_folder, url)
