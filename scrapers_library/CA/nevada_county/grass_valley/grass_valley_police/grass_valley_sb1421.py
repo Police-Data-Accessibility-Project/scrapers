0c1070ef321da29d3de50a54bf07412e9300e340
Kylie <github.a5cee@kyscott.aleeas.com>
2023-10-30 14:38:52 -0600

    Retrieve video files for download from Vimeo

--- scrapers_library/CA/nevada_county/grass_valley/grass_valley_police/grass_valley_sb1421.py
+++ scrapers_library/CA/nevada_county/grass_valley/grass_valley_police/grass_valley_sb1421.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 import requests
 from vimeo_downloader import Vimeo
@@ -9,13 +10,6 @@ sys.path.insert(1, str(p))
 
 from utils.pdf.list_pdf_scrapers.single_pdf_scraper import single_pdf_scraper
 
-'''
-url = "https://player.vimeo.com/video/702224442"
-embedded_on = 'https://www.cityofgrassvalley.com/post/g1901078'
-v = Vimeo(url, embedded_on)
-stream = v.streams
-stream[-1].download(download_directory='./test', filename='test5')
-'''
 
 '''
 file_url = "https://cityofgrassvalley-my.sharepoint.com/%3Ab%3A/g/personal/bkalstein_gvpd_net/EaVFsdEin1FMiUaf8SklOoAB51Bt8shEF376SWVHrgUgXA?e=RDd9VP"
@@ -35,28 +29,67 @@ with open(file_path, "wb") as fd:
 '''
 
 def get_vimeo_videos():
+    embedded_on = "https://www.cityofgrassvalley.com/"
     videos = [
-        [
-            ""
-        ]
+        {
+            "dir": "./data/G2000004",
+            "url": "https://player.vimeo.com/video/395570204"
+        },
+        {
+            "dir": "./data/G2000004",
+            "url": "https://player.vimeo.com/video/394047232"
+        },
+        {
+            "dir": "./data/G2000004",
+            "url": "https://player.vimeo.com/video/394046683"
+        },
+        {
+            "dir": "./data/G2000004",
+            "url": "https://player.vimeo.com/video/394048251"
+        },
+        {
+            "dir": "./data/G2000004",
+            "url": "https://player.vimeo.com/video/685656476"
+        },
+        {
+            "dir": "./data/G1901078",
+            "url": "https://player.vimeo.com/video/702224442"
+        },
+        {
+            "dir": "./data/G1901078",
+            "url": "https://player.vimeo.com/video/702221548"
+        }
     ]
+
+    print("\nRetrieving video files...")
+    for video in videos:
+        v = Vimeo(video["url"], embedded_on)
+        stream = v.streams
+
+        if os.path.exists(f"{video['dir']}/{stream[-1].title}.mp4"):
+            continue
+        
+        stream[-1].download(download_directory=video["dir"])
 
 
 def main():
     pdfs = [
         {
-            "save_dir": "./data/G2300034/",
+            "dir": "./data/G2300034/",
             "url": "https://www.cityofgrassvalley.com/sites/main/files/file-attachments/g2300034_-_incident_report.pdf"
         },
         {
-            "save_dir": "./data/G1901078/",
+            "dir": "./data/G1901078/",
             "url": "https://www.cityofgrassvalley.com/sites/main/files/file-attachments/g1901078_ir_redacted.pdf"
         }
     ]
 
     for pdf in pdfs:
-        single_pdf_scraper(pdf["save_dir"], pdf["url"])
-    
+        single_pdf_scraper(pdf["dir"], pdf["url"])
+
+    if not os.path.exists("./data/G2000004/"):
+        os.makedirs("./data/G2000004/")
+
     get_vimeo_videos()
 
 
