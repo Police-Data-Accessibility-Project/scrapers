f4d9657bd5598e9e9b85b3094b68fba809c3eb4c
Kylie <github.a5cee@kyscott.aleeas.com>
2023-11-09 16:42:40 -0700

    Structure to retrieve PDFs

---
+++ scrapers_library/CA/san_diego_county/la_mesa/la_mesa_police/content.py
@@ -0,0 +1,125 @@
+videos = [
+    {
+        "name": "Purdy Protest.ts",
+        "url": "https://wms.civplus.tikiliveapi.com/rokuvod_civplus/145063/smil:civplus/encoded_streams/0/385/145063.smil/chunklist_w993471234_b4319633.m3u8",
+        "dir": "./data/CR 20-015724/"
+    },
+    {
+        "name": "Gay 1167.ts",
+        "url": "https://wms.civplus.tikiliveapi.com/rokuvod_civplus/145072/smil:civplus/encoded_streams/0/385/145072.smil/chunklist_w1378324341_b4811709.m3u8",
+        "dir": "./data/CR 20-015724/"
+    },
+    {
+        "name": "Clip Gay 1167.ts",
+        "url": "https://wms.civplus.tikiliveapi.com/rokuvod_civplus/145064/smil:civplus/encoded_streams/0/385/145064.smil/chunklist_w1606020957_b4542114.m3u8",
+        "dir": "./data/CR 20-015724/"
+    },
+    {
+        "name": "ABLE Video.ts",
+        "url": "https://wms.civplus.tikiliveapi.com/rokuvod_civplus/145113/smil:civplus/encoded_streams/0/385/145113.smil/chunklist_w279067724_b2993828.m3u8",
+        "dir": "./data/CR 20-015724/"
+    },
+    {
+        "name": "Knudson Rock.ts",
+        "url": "https://wms.civplus.tikiliveapi.com/rokuvod_civplus/145060/smil:civplus/encoded_streams/0/385/145060.smil/chunklist_w32458377_b5001171.m3u8",
+        "dir": "./data/CR 20-015724/"
+    },
+    {
+        "name": "Knudson Protest.ts",
+        "url": "https://wms.civplus.tikiliveapi.com/rokuvod_civplus/145062/smil:civplus/encoded_streams/0/385/145062.smil/chunklist_w1492225134_b4589629.m3u8",
+        "dir": "./data/CR 20-015724/"
+    },
+    {
+        "name": "Purdy-Knudson Rock.ts",
+        "url": "https://wms.civplus.tikiliveapi.com/rokuvod_civplus/145059/smil:civplus/encoded_streams/0/385/145059.smil/chunklist_w1242602046_b4685332.m3u8",
+        "dir": "./data/CR 20-015724/"
+    },
+    {
+        "name": "Purdy Rocks 1.ts",
+        "url": "https://wms.civplus.tikiliveapi.com/rokuvod_civplus/145068/smil:civplus/encoded_streams/0/385/145068.smil/chunklist_w2056206713_b4545757.m3u8",
+        "dir": "./data/CR 20-015724/"
+    },
+    {
+        "name": "Purdy Rocks 2.ts",
+        "url": "https://wms.civplus.tikiliveapi.com/rokuvod_civplus/145069/smil:civplus/encoded_streams/0/385/145069.smil/chunklist_w1416629678_b4349568.m3u8",
+        "dir": "./data/CR 20-015724/"
+    },
+    {
+        "name": "Purdy Rocks 3.ts",
+        "url": "https://wms.civplus.tikiliveapi.com/rokuvod_civplus/145070/smil:civplus/encoded_streams/0/385/145070.smil/chunklist_w1816692435_b5543447.m3u8",
+        "dir": "./data/CR 20-015724/"
+    },
+    {
+        "name": "Purdy Rocks 4.ts",
+        "url": "https://wms.civplus.tikiliveapi.com/rokuvod_civplus/145056/smil:civplus/encoded_streams/0/385/145056.smil/chunklist_w892800543_b5041400.m3u8",
+        "dir": "./data/CR 20-015724/"
+    },
+    {
+        "name": "Purdy Rocks 5.ts",
+        "url": "https://wms.civplus.tikiliveapi.com/rokuvod_civplus/145057/smil:civplus/encoded_streams/0/385/145057.smil/chunklist_w1335284243_b4214453.m3u8",
+        "dir": "./data/CR 20-015724/"
+    },
+    {
+        "name": "Facebook Live 1.ts",
+        "url": "https://wms.civplus.tikiliveapi.com/rokuvod_civplus/145066/smil:civplus/encoded_streams/0/385/145066.smil/chunklist_w1907741385_b373052.m3u8",
+        "dir": "./data/CR 20-015724/"
+    },
+    {
+        "name": "Facebook Live 2.ts",
+        "url": "https://wms.civplus.tikiliveapi.com/rokuvod_civplus/145067/smil:civplus/encoded_streams/0/385/145067.smil/chunklist_w932188010_b332927.m3u8",
+        "dir": "./data/CR 20-015724/"
+    },
+    {
+        "name": "Samp Rocks 1.ts",
+        "url": "https://wms.civplus.tikiliveapi.com/rokuvod_civplus/145058/smil:civplus/encoded_streams/0/385/145058.smil/chunklist_w2086652848_b4200413.m3u8",
+        "dir": "./data/CR 20-015724/"
+    },
+    {
+        "name": "Samp Rocks 2.ts",
+        "url": "https://wms.civplus.tikiliveapi.com/rokuvod_civplus/145071/smil:civplus/encoded_streams/0/385/145071.smil/chunklist_w1985358365_b3933032.m3u8",
+        "dir": "./data/CR 20-015724/"
+    },
+    {
+        "name": "Station Camera 1.ts",
+        "url": "https://wms.civplus.tikiliveapi.com/rokuvod_civplus/145114/smil:civplus/encoded_streams/0/385/145114.smil/chunklist_w1679926162_b380475.m3u8",
+        "dir": "./data/CR 20-015724/"
+    },
+    {
+        "name": "Station Camera 2.ts",
+        "url": "https://wms.civplus.tikiliveapi.com/rokuvod_civplus/145115/smil:civplus/encoded_streams/0/385/145115.smil/chunklist_w1650835485_b123730.m3u8",
+        "dir": "./data/CR 20-015724/"
+    },
+    {
+        "name": "Witness Cellphone Video.ts",
+        "url": "https://wms.civplus.tikiliveapi.com/rokuvod_civplus/145088/smil:civplus/encoded_streams/0/385/145088.smil/chunklist_w2134352645_b3363863.m3u8",
+        "dir": "./data/CR 20-015724/"
+    },
+]
+
+pdfs = [
+    {
+        "name": "Helicopter Images.pdf",
+        "url": "https://www.cityoflamesa.us/DocumentCenter/View/17169/Helicopter-Images",
+        "dir": "./data/CR 20-015724/"
+    },
+    {
+        "name": "Photos.pdf",
+        "url": "https://www.cityoflamesa.us/DocumentCenter/View/17170/Photos",
+        "dir": "./data/CR 20-015724/"
+    },
+    {
+        "name": "SDSO Scene Photos 1.pdf",
+        "url": "https://www.cityoflamesa.us/DocumentCenter/View/17171/SDSO-Scene-Photos-1",
+        "dir": "./data/CR 20-015724/"
+    },
+    {
+        "name": "SDSO Scene Photos 2.pdf",
+        "url": "https://www.cityoflamesa.us/DocumentCenter/View/17172/SDSO-Scene-Photos-2",
+        "dir": "./data/CR 20-015724/"
+    },
+    {
+        "name": "SDSO Scene Photos 3.pdf",
+        "url": "https://www.cityoflamesa.us/DocumentCenter/View/17173/SDSO-Scene-Photos-3",
+        "dir": "./data/CR 20-015724/"
+    },
+]
--- scrapers_library/CA/san_diego_county/la_mesa/la_mesa_police/la_mesa_sb1421.py
+++ scrapers_library/CA/san_diego_county/la_mesa/la_mesa_police/la_mesa_sb1421.py
@@ -5,6 +5,7 @@ import re
 import os
 from concurrent.futures import as_completed, ThreadPoolExecutor
 from tqdm import tqdm
+from content import videos, pdfs
 
 '''
 def strip_end(text, suffix):
@@ -13,47 +14,72 @@ def strip_end(text, suffix):
     return text[:len(text)-len(suffix)]
 '''
 
-def get_ts_stream():
-    m3u8_url = "https://wms.civplus.tikiliveapi.com/rokuvod_civplus/145062/smil:civplus/encoded_streams/0/385/145062.smil/chunklist_w1492225134_b4589629.m3u8"
+def get_ts_stream(m3u8_url, savedir, filename):
+    if os.path.exists(savedir + filename):
+        return
+
     r = requests.get(m3u8_url)
     m3u8_master = m3u8.loads(r.text)
     m3u8_file = m3u8_url.split("/")[-1]
     url = m3u8_url[:len(m3u8_url)-len(m3u8_file)]
 
-    if not os.path.exists("ts_files"):
-        os.makedirs("ts_files")
+    TS_DIR = "./ts_files/"
+    shutil.rmtree(TS_DIR, ignore_errors=True)
 
     results = []
     with ThreadPoolExecutor(max_workers=6) as executor:
-        future_to_url = [executor.submit(download_file, url + seg["uri"]) for seg in m3u8_master.data["segments"]]
+        future_to_url = [executor.submit(download_file, url + seg["uri"], TS_DIR) for seg in m3u8_master.data["segments"]]
 
-        for future in tqdm(as_completed(future_to_url), total=len(future_to_url), desc="Clip11Knudson1221Protest.ts"):
+        for future in tqdm(as_completed(future_to_url), total=len(future_to_url), desc=filename):
             data = future.result()
             results.append(data)
 
-    TS_DIR = "./ts_files"
-    with open("Clip11Knudson1221Protest.ts", "wb") as video:
+    os.makedirs(savedir, exist_ok=True)
+    
+    with open(savedir + filename, "wb") as video:
         dir_list = os.listdir(TS_DIR)
         dir_list.sort(key=lambda f: int("".join(filter(str.isdigit, f))))
 
         for ts_file in tqdm(dir_list, desc="Merging segments"):
-            with open(f"{TS_DIR}/{ts_file}", "rb") as mergefile:
+            with open(TS_DIR + ts_file, "rb") as mergefile:
                 shutil.copyfileobj(mergefile, video)
 
     shutil.rmtree(TS_DIR)
 
 
-def download_file(url):
-    local_filename = url.split("/")[-1]
+def download_file(url, savedir, filename=None, show_status=False):
+    if filename is None:
+        filename = url.split("/")[-1]
+    
+    if os.path.exists(savedir + filename):
+        return
+
+    os.makedirs(savedir, exist_ok=True)
+
     r = requests.get(url, stream=True)
 
-    with open(f"ts_files/{local_filename}", "wb") as f:
+    if show_status:
+        total = int(r.headers.get('content-length', 0))
+        progress_bar = tqdm(total=total, unit='iB', unit_scale=True, desc=filename)
+
+    with open(savedir + filename, "wb") as f:
         for chunk in r.iter_content(chunk_size=1024):
+            if show_status:
+                progress_bar.update(len(chunk))
             f.write(chunk)
+    
+    if show_status:
+        progress_bar.close()
 
 
 def main():
-    get_ts_stream()
+    print("Retrieving video files...")
+    for video in videos:
+        get_ts_stream(video["url"], video["dir"], video["name"])
+
+    print("Retrieving PDF files...")
+    for pdf in pdfs:
+        download_file(pdf["url"], pdf["dir"], pdf["name"], show_status=True)
 
 
 if __name__ == "__main__":
