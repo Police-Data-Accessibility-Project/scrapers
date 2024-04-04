cd7af3e717ecc0a869860f6911872069930048d0
Michael DePace <michaeldepacejr@gmail.com>
2024-04-04 00:56:13 -0400

    cleaned up scraper code

--- scrapers_library/PA/court_cases/scraper.py
+++ scrapers_library/PA/court_cases/scraper.py
@@ -1,11 +1,18 @@
 import requests
-#supabase
-#local env 
+import json
 
-url="https://services.pacourts.us/public/v1/cases/MJ-05213-LT-0000011-2021"
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'}
-#what is user agent
+url="https://services.pacourts.us/public/v1/cases/"
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'} # prevents request from being denied by api
 
+# "MJ-05213-LT-0000011-2021"
 
-resp = requests.get(url, headers=headers)
-print(resp.json())
+def pull_case_json(case_id):
+    resp = requests.get(url + case_id, headers=headers)
+    filename = "./data/" + case_id + ".json"
+
+    with open(filename, 'w') as f:
+        json.dump(resp.json(), f) # save json to local file
+
+#pull_case_json("MJ-09302-TR-0000928-2020")
+#pull_case_json("MJ-06306-TR-0000813-2020")
+#pull_case_json("MJ-51301-TR-0000779-2020")
