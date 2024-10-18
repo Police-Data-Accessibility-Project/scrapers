816e4eac74b49c61339ef8c71d29ed906450399f
Kylie <github.a5cee@kyscott.aleeas.com>
2024-10-18 15:52:18 -0600

    Basic prototype for package retrieval

--- scrapers_library/data_portals/ckan/ckan_scraper.py
+++ scrapers_library/data_portals/ckan/ckan_scraper.py
@@ -1,52 +1,18 @@
-#import pprint
-#import requests
 import json
-#import pandas 
-#from colorama import Fore, Style
 import sys
+
 from ckanapi import RemoteCKAN
 
-remote = RemoteCKAN("https://catalog.data.gov/", get_only=True)
-packages = remote.action.package_list(fq="tags:police", rows=1000)
-print(len(packages["results"]))
-input()
 
 
-try:
-    packages = '/api/3/action/package_list?fq=tags:police&rows=10000'
-    packagesPath = userDataPortal + packages
-    print('The full URL created is: ', packagesPath)
-    #Let's make the request for the package list from the desired portal URL
-    respose = requests.get(packagesPath)
-except:
-    print(Fore.RED + 'An error occured with the base URL you provided. Please try again with a base URL.')
-    sys.exit()
+def ckan_package_search(base_url, query=None, **kwargs):
+    remote = RemoteCKAN(base_url, get_only=True)
+    packages = remote.action.package_search(q=query, rows=1000, start=1000, **kwargs)
+    print(len(packages["results"]))
+    print(packages["count"])
+    for key in packages.keys():
+        print(key)
 
 
-
-#Using the JSON module to load the response into a dictionary data structure
-response_dict = json.loads(respose.content)
-
-#Check if the contents from the response is valid JSON
-try:
-    assert response_dict['success'] is True
-except AssertionError:
-    print("The response was not successful.")
-
-#Titles of the datasets are the key value in the dictionary; this will provide a list of those dataset names
-#This extracts just the dataset names from the dictionary data structure
-datasets = response_dict['result']['results']
-
-#This prints the names (off by default) of the number of datasets and then the number of datasets (optional - commented out by default)
-print("The number of datasets discovered is: ")
-packageCount = len(datasets)
-print(Fore.YELLOW + "", packageCount)
-print("The following datasets were found at the Data Portal you provided: \n")
-
-for key in datasets:
-    continue
-    print(key)
-
-#This is to help provide degugging information as to the type of data the datasets variable is (list vs. dictionary vs. dataframe, etc)
-print(type(datasets))
-
+if __name__ == "__main__":
+    ckan_package_search(base_url="https://catalog.data.gov/", query="police")
