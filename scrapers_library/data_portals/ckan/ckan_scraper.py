1d22426f76c214eb27e0bc33a3db7ab20d9f71dc
Kylie <github.a5cee@kyscott.aleeas.com>
2024-10-18 14:11:42 -0600

    Cleanup and test new methodology

--- scrapers_library/data_portals/ckan/ckan_scraper.py
+++ scrapers_library/data_portals/ckan/ckan_scraper.py
@@ -1,34 +1,19 @@
-'''W. Alec Akin
-Police Data Accessibility Project
-PDAP CKAN Python Project
-main.py
-April 13, 2022
-https://pdap.io
-Based on information from https://www.pythonsherpa.com/tutorials/2/
-Intended for use with https://data.wprdc.org/dataset/officer-training
-and other CKAN-based Open Data Portals'''
-
-
-#Import libraries required
-import pprint
-import requests
-import json     #Allows us to work with JSON objects
-import pandas   #Popular data science libraries
-from colorama import Fore, Style #Allows color coded responses to terminal/stdout
-import welcome
+#import pprint
+#import requests
+import json
+#import pandas 
+#from colorama import Fore, Style
 import sys
+from ckanapi import RemoteCKAN
 
-#Give us a fancy welcome banner :)
-welcome.welcomeBanner()
+remote = RemoteCKAN("https://catalog.data.gov/", get_only=True)
+packages = remote.action.package_list(fq="tags:police", rows=1000)
+print(len(packages["results"]))
+input()
 
-#Get root URL of CKAN Data Portal we want to get data from
-print(Fore.LIGHTBLUE_EX + 'Please enter the base URL of the data portal you want to connect to: ')
-userDataPortal = input()
-
-#Define packages location at URL path
 
 try:
-    packages = '/api/3/action/package_list'
+    packages = '/api/3/action/package_list?fq=tags:police&rows=10000'
     packagesPath = userDataPortal + packages
     print('The full URL created is: ', packagesPath)
     #Let's make the request for the package list from the desired portal URL
@@ -50,7 +35,7 @@ except AssertionError:
 
 #Titles of the datasets are the key value in the dictionary; this will provide a list of those dataset names
 #This extracts just the dataset names from the dictionary data structure
-datasets = response_dict['result']
+datasets = response_dict['result']['results']
 
 #This prints the names (off by default) of the number of datasets and then the number of datasets (optional - commented out by default)
 print("The number of datasets discovered is: ")
@@ -59,6 +44,7 @@ print(Fore.YELLOW + "", packageCount)
 print("The following datasets were found at the Data Portal you provided: \n")
 
 for key in datasets:
+    continue
     print(key)
 
 #This is to help provide degugging information as to the type of data the datasets variable is (list vs. dictionary vs. dataframe, etc)
